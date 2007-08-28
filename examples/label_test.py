79896ce14f65a6ddaca3ee69c6e45446d66e1ee8
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-28 23:23:22 +0000

    removing RGBAColorTrait and editor from traits and traits ui; removing RGBAColor as well; changing all references to these (in VET, some traits UI demos, etc.) to reference the RGBAColor trait defined in Enable2; fixing a small versioning error in block canvas's setup.py; adding label_test demo to enable2

---
+++ examples/label_test.py
@@ -0,0 +1,18 @@
+""" Small demo of the Label component.  """
+
+from enthought.enable2.example_support import DemoFrame, demo_main
+from enthought.enable2.label import Label
+
+from enthought.enable2.api import Component, Container, Pointer, Window
+
+class MyFrame(DemoFrame):
+    
+    def _create_window(self):
+        label = Label(bounds=[100, 50], position=[50,50], text="HELLO")
+        label.bgcolor = "red"
+        return Window(self, -1, component=label)
+
+if __name__ == "__main__":
+    demo_main(MyFrame, title="Click and drag to move the box")
+
+# EOF
