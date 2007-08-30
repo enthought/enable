2b81c450ab7d3c84277a1c1d3e5b7cb00a09d3f3
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-30 00:04:28 +0000

    Checking in canvas demo.  Also adding commit message for revision 13951, which didn't have one because of user error.
    Commit message for 13951:
    - adding first cut of unbounded Canvas object
    - adding support to Viewport for updating view_bounds on the Canvas object
    - moved BaseTool from Chaco import Enable
    - created enable2/tools package and copied over TraitsTool, PanTool, and DragTool into Enable
    - modified TraitsTool to not be plotting-specific, fixed imports on the others
    - renamed the _draw method on primitives/box.py
    - removed check for whether or not self.compact() should be called after component removal in Container; this logic apparently never worked anyway
    - changed logic in Container.__init__() to only stomp on the deprecated 'auto_size' trait if the user passed in bounds at object creation time; this prevents it from overriding the value for auto_size that a subclass may have defined at the traits level (in the class definition)

---
+++ examples/canvas_demo.py
@@ -0,0 +1,23 @@
+
+
+from enthought.enable2.api import Canvas, Viewport, Window
+from enthought.enable2.primitives.api import Box
+from enthought.enable2.example_support import demo_main, DemoFrame
+
+class MyFrame(DemoFrame):
+
+    def _create_window(self):
+
+        canvas = Canvas()
+        box = Box(color="red", bounds=[50,50], resizable="")
+        box.position= [75,75]
+        canvas.add(box)
+
+
+        viewport = Viewport(component=canvas)
+        viewport.view_position = [0,0]
+
+        return Window(self, -1, component=viewport)
+
+if __name__ == "__main__":
+    demo_main(MyFrame, title="Canvas example")
