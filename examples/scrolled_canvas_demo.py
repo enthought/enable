84e6ca493615c7e7c3ea5a76b6a13b8e830281dc
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-09-08 00:23:40 +0000

    updating the scrolled canvas demo.  It's got a lot more boxes now!

--- examples/scrolled_canvas_demo.py
+++ examples/scrolled_canvas_demo.py
@@ -1,4 +1,5 @@
 
+from numpy import arange, array
 
 from enthought.enable2.api import Canvas, Viewport, Window, Scrolled
 from enthought.enable2.tools.api import ViewportPanTool
@@ -10,21 +11,37 @@ class MyFrame(DemoFrame):
     def _create_window(self):
 
         canvas = Canvas(bgcolor="lightsteelblue", draw_axes=True)
-        from basic_move import Box
-        box = Box(color="red", bounds=[50,50], resizable="")
-        box.position= [75,75]
-        canvas.add(box)
-
+        
+        boxgridsize = 8
+        boxsize = 50
+        
+        spacing = boxsize * 2
+        offset = spacing / 2
+        
+        origin_color = array([0.0, 0.0, 1.0])
+        x_color = array([0.0, 1.0, 0.0])
+        y_color = array([1.0, 0.0, 0.0])
+        
+        for i in range(boxgridsize):
+            for j in range(boxgridsize):
+                color = tuple(x_color / (boxgridsize - 1) * i + \
+                              y_color / (boxgridsize - 1) * j + \
+                              origin_color) + (1.0,)
+                box = Box(color=color, bounds=[boxsize, boxsize], resizable="")
+                box.position= [i*spacing + offset - boxsize/2 + 0.5,
+                               j*spacing + offset - boxsize/2 + 0.5]
+                canvas.add(box)
 
         viewport = Viewport(component=canvas)
         viewport.view_position = [0,0]
         viewport.tools.append(ViewportPanTool(viewport))
 
-
         scrolled = Scrolled(canvas, fit_window = True,
                             inside_padding_width = 0,
                             mousewheel_scroll = False,
-                            viewport_component = viewport)
+                            viewport_component = viewport,
+                            always_show_sb = True,
+                            continuous_drag_update = False)
 
         return Window(self, -1, component=scrolled)
 
