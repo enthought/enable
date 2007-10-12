e81a768720d0b0aef760e0f52638251e54ed5417
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-10-12 03:24:43 +0000

    - adding support for layout to AbstractOverlay - cleaning up viewport_toolbar and refactoring to use layout - adding fix for first toolbar button label rendering incorrectly when the viewport is scaled - adding call to superclass constructor in ViewportToolbar - refactoring toolbar buttons, removing some unused traits

--- enthought/enable2/abstract_overlay.py
+++ enthought/enable2/abstract_overlay.py
@@ -29,15 +29,49 @@ class AbstractOverlay(Component):
     # Typically, an overlay does not render a background.
     bgcolor = "transparent"
 
-    def __init__(self, component=None, *args, **kw):
-        if component is not None:
-            self.component = component
-        super(AbstractOverlay, self).__init__(*args, **kw)
+    #----------------------------------------------------------------------
+    # Abstract methods (to be implemented by subclasses)
+    #----------------------------------------------------------------------
 
     def overlay(self, other_component, gc, view_bounds=None, mode="normal"):
         """ Draws this component overlaid on another component.
         """
+        # Subclasses should implement this method.
         pass
+
+    def _do_layout(self, component=None):
+        """ Called by do_layout() to do an actual layout call; it bypasses some
+        additional logic to handle null bounds and setting **_layout_needed**.
+        """
+        pass
+
+
+    #----------------------------------------------------------------------
+    # Concrete methods / reimplementations of Component methods
+    #----------------------------------------------------------------------
+
+    def __init__(self, component=None, *args, **kw):
+        if component is not None:
+            self.component = component
+        super(AbstractOverlay, self).__init__(*args, **kw)
+    
+    def do_layout(self, size=None, force=False, component=None):
+        """ Tells this component to do a layout at a given size.  This differs
+        from the superclass Component.do_layout() in that it accepts an
+        optional **component** argument.
+        """
+        if self._layout_needed or force:
+            if size is not None:
+                self.bounds = size
+            self._do_layout(component)
+            self._layout_needed = False
+        for underlay in self.underlays:
+            if underlay.visible or underlay.invisible_layout:
+                underlay.do_layout(component)
+        for overlay in self.overlays:
+            if overlay.visible or overlay.invisible_layout:
+                overlay.do_layout(component)
+        return
 
     def _draw(self, gc, view_bounds=None, mode="normal"):
         """ Draws the component, paying attention to **draw_order**.
