2b55a751b13edb9b18944359caf1005c1c000b94
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-01-24 17:58:54 +0000

    adding pyglet-based demo of Kiva's GL backend

---
+++ examples/kiva/pyglet_gl.py
@@ -0,0 +1,53 @@
+
+
+from numpy import array
+
+import pyglet
+from pyglet.window import key, Window
+from enthought.kiva.backend_gl import GraphicsContext
+from enthought.kiva.constants import FILL, STROKE, FILL_STROKE
+
+class TestWindow(Window):
+    def __init__(self, *args, **kw):
+        Window.__init__(self, *args, **kw)
+        self.init_window()
+
+    def init_window(self):
+        self.gc = GraphicsContext(size=(self.width, self.height))
+        self.gc.init_gl_viewport()
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol in (key.ESCAPE, key.Q):
+            self.has_exit = True
+    
+    def draw(self):
+        gc = self.gc
+        gc.save_state()
+        gc.clear(0, 1, 0, 1)
+        gc.set_stroke_color((1,1,1,1))
+        gc.set_line_width(2)
+        pts = array([[50, 50], [50,100], [100,100], [100,50]])
+        gc.begin_path()
+        gc.lines(pts)
+        gc.close_path()
+        gc.draw_path(STROKE)
+        gc.flush()
+        gc.restore_state()
+
+
+def main():
+    win = TestWindow(width = 640, height=480)
+    exit = False
+    while not exit:
+        win.switch_to()
+        win.dispatch_events()
+        win.clear()
+        win.draw()
+        win.flip()
+        exit = win.has_exit
+
+
+if __name__ == "__main__":
+    main()
+
+
