5716786932a7adf6640fdd00a8b079716373d8a9
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-16 19:58:58 +0000

    Fixed imports

--- examples/kiva/agg/examples/polygon_hit_test.py
+++ examples/kiva/agg/examples/polygon_hit_test.py
@@ -1,13 +1,11 @@
-def rand(*args):
-    from enthought.util.randomx import random
-    return random(args)
-from enthought.util.numerix import *
+import numpy
+
 from enthought.kiva import agg
 
-poly = array(((0.0,   0.0),
-              (10.0,  0.0),
-              (10.0, 10.0),
-              ( 0.0, 10.0)))
+poly = numpy.array((( 0.0,  0.0),
+                    (10.0,  0.0),
+                    (10.0, 10.0),
+                    ( 0.0, 10.0)))
 
 print agg.point_in_polygon(-1,-1,poly)
 print agg.point_in_polygon(0.0,0.0,poly)
@@ -15,18 +13,19 @@ print agg.point_in_polygon(5,5,poly)
 print agg.point_in_polygon(10,10,poly)
 print agg.point_in_polygon(15,15,poly)
 
-pts = array(((-1.0, -1.0),
-             ( 0.1,  0.0),
-             ( 0.0,  0.1),
-             ( 0.0,  0.0),
-             ( 5.0,  5.0),
-             ( 10.0, 10.0),
-             ( 15.0, 15.0)))
+pts = numpy.array(((-1.0, -1.0),
+                   ( 0.1,  0.0),
+                   ( 0.0,  0.1),
+                   ( 0.0,  0.0),
+                   ( 5.0,  5.0),
+                   ( 10.0, 10.0),
+                   ( 15.0, 15.0)))
+
 results = agg.points_in_polygon(pts, poly)
 
 print results
 
-pts = rand(20000, 2)*12.5-2.5
+pts = numpy.random.random_sample((20000, 2))*12.5-2.5
 
 import time
 t1 = time.clock()
@@ -37,19 +36,19 @@ print 'points_in_polygon() for %d pts in %d point polygon (sec): %f' % \
 print pts[:5]
 print results[:5]
 
-poly = array((( 0.0,  0.0),
-              ( 2.0,  0.0),
-              ( 5.0,  0.0),
-              ( 7.5,  0.0),
-              (10.0,  0.0),
-              (10.0,  2.5),
-              (10.0,  5.0),
-              (10.0,  7.5),
-              (10.0, 10.0),
-              ( 7.5, 10.0),
-              ( 5.0, 10.0),
-              ( 2.5, 10.0),
-              ( 0.0, 10.0)))
+poly = numpy.array((( 0.0,  0.0),
+                    ( 2.0,  0.0),
+                    ( 5.0,  0.0),
+                    ( 7.5,  0.0),
+                    (10.0,  0.0),
+                    (10.0,  2.5),
+                    (10.0,  5.0),
+                    (10.0,  7.5),
+                    (10.0, 10.0),
+                    ( 7.5, 10.0),
+                    ( 5.0, 10.0),
+                    ( 2.5, 10.0),
+                    ( 0.0, 10.0)))
 
 t1 = time.clock()
 results = agg.points_in_polygon(pts, poly)
