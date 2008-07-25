30a4281f2602bd6b9d90d89375785a2645854a0d
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 20:26:26 +0000

    Improve the proxy module which maps to a module which uses __all__.
    The notes I made in the code apply to all proxy modules which map
    to a module which uses __all__.

--- enthought/enable2/pyglet_backend/pyglet_app.py
+++ enthought/enable2/pyglet_backend/pyglet_app.py
@@ -1,2 +1,19 @@
-# proxy    
+# proxy
+
+__all__ = ["get_app", "PygletApp"]
+
 from enthought.enable.pyglet_backend.pyglet_app import *
+
+
+# Import the objects which are not declared in __all__,
+# but are still defined in the real module, such that people
+# can import them explicitly when needed, just as they could
+# with the real module.
+#
+# It is unlikely that someone will import these objects, since
+# they start with '_'.  However, the proxy's job is to mimic the
+# behavior of the real module as closely as possible.
+# The proxy's job is not to define or change the API.
+#
+from enthought.enable.pyglet_backend.pyglet_app import _CurrentApp, _PygletApp
+
