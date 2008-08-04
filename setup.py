4e540b8275e76e9c8416665de2ca3c5bf00ae9a7
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-04 19:03:04 +0000

    Fix Enable setup.py to work around a setuptools bug which gets setup_data.py metadata from incorrect packages.

--- setup.py
+++ setup.py
@@ -74,11 +74,16 @@ from make_docs import HtmlBuild
 from numpy.distutils.core import setup
 from pkg_resources import DistributionNotFound, parse_version, require, \
     VersionConflict
-from setup_data import INFO
 import numpy
 import os
 import zipfile
 
+# FIXME: This works around a setuptools bug which gets setup_data.py metadata
+# from incorrect packages. Ticket #1592
+#from setup_data import INFO
+setup_data = dict(__name__='', __file__='setup_data.py')
+execfile('setup_data.py', setup_data)
+INFO = setup_data['INFO']
 
 # Pull the description values for the setup keywords from our file docstring.
 DOCLINES = __doc__.split("\n")
