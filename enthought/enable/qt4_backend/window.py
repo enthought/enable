3269a60a6514cbb680afa3d07268289aa731997a
phil <phil@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-18 11:16:48 +0000

    Possibly fixed a problem with the Qt backend getting a non-widget parent.

--- enthought/enable/qt4_backend/window.py
+++ enthought/enable/qt4_backend/window.py
@@ -27,8 +27,7 @@ class _QtWindow(QtGui.QWidget):
     """ The Qt widget that implements the enable control. """
 
     def __init__(self, enable_window, parent):
-        #QtGui.QWidget.__init__(self, parent)
-        super(_QtWindow, self).__init__(parent)
+        QtGui.QWidget.__init__(self)
 
         self._enable_window = enable_window
 
