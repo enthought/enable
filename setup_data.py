4e6ec116fafa3aa20dce7d2b6a18933fc4f25370
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-22 21:28:23 +0000

    updating enable's setup_data.py to work with ETS 3.0.2 versions of traits and EnthoughtBase

--- setup_data.py
+++ setup_data.py
@@ -15,10 +15,10 @@ def etsdep(p, min, max=None, literal=False):
 
 # Declare our ETS project dependencies.
 #CHACO -- not ever imported here, the etscollect tool is messing up somehow.
-ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.1')
+ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.0')
 TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.3')
-TRAITSGUI = etsdep('TraitsGUI', '3.0.3')
-TRAITS_UI = etsdep('Traits[ui]', '3.0.3')
+TRAITSGUI = etsdep('TraitsGUI', '3.0.2')
+TRAITS_UI = etsdep('Traits[ui]', '3.0.2')
 
 
 # A dictionary of the setup data information.
