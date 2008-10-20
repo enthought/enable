7323cf694b6727be080439fa28693b87e8e04f5f
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-10-20 17:50:20 +0000

    part of a refactor step to move basic toolbar from BlockCanvas to Enable

---
+++ enthought/enable/tools/toolbars/toolbar_buttons.py
@@ -0,0 +1,154 @@
+# Enthought library imports
+from enthought.enable.api import ColorTrait, Component
+from enthought.kiva import font_metrics_provider
+from enthought.kiva.traits.kiva_font_trait import KivaFont
+from enthought.traits.api import Bool, Enum, Instance, Int, Property, Str, Tuple
+
+class Button(Component):
+    
+    color = ColorTrait((0.6, 0.6, 0.6, 1.0))
+    
+    down_color = ColorTrait("gray")
+    
+    border_color = ColorTrait((0.4, 0.4, 0.4, 1.0))
+    
+    # important for rendering rounded buttons properly, since the default for
+    # the Component parent class is 'white'
+    bgcolor = "clear"
+    
+    label = Str
+    
+    label_font = KivaFont("modern 11 bold")
+    
+    label_color = ColorTrait("white")
+    
+    label_shadow = ColorTrait("gray")
+
+    shadow_text = Bool(True)
+
+    label_padding = Int(5)
+    
+    height = Int(20)
+
+    button_state = Enum("up", "down")
+
+    end_radius = Int(10)
+
+    # Default size of the button if no label is present
+    bounds=[32,32]
+
+    # Cached value of the measured sizes of self.label
+    _text_extents = Tuple
+    
+    def perform(self, event):
+        """
+        Called when the button is depressed.  'event' is the Enable mouse event
+        that triggered this call.
+        """
+        pass
+
+    def _draw_mainlayer(self, gc, view_bounds, mode="default"):
+        if self.button_state == "up":
+            self.draw_up(gc, view_bounds)
+        else:
+            self.draw_down(gc, view_bounds)
+        return
+
+    def draw_up(self, gc, view_bounds):
+        gc.save_state()
+        try:
+            gc.set_fill_color(self.color_)
+            self._draw_actual_button(gc)
+        finally:
+            gc.restore_state()
+        return
+
+    def draw_down(self, gc, view_bounds):
+        gc.save_state()
+        try:
+            gc.set_fill_color(self.down_color_)
+            self._draw_actual_button(gc)
+        finally:
+            gc.restore_state()
+        return
+
+    def _draw_actual_button(self, gc):
+        gc.set_stroke_color(self.border_color_)
+        gc.begin_path()
+        
+        gc.move_to(self.x + self.end_radius, self.y)
+        
+        gc.arc_to(self.x + self.width, self.y, 
+                self.x + self.width, 
+                self.y + self.end_radius, self.end_radius)
+        gc.arc_to(self.x + self.width, 
+                self.y + self.height,
+                self.x + self.width - self.end_radius,
+                self.y + self.height, self.end_radius)
+        gc.arc_to(self.x, self.y + self.height,
+                self.x, self.y,
+                self.end_radius)
+        gc.arc_to(self.x, self.y,
+                self.x + self.width + self.end_radius,
+                self.y, self.end_radius)
+
+        gc.draw_path()
+        self._draw_label(gc)
+
+    def _draw_label(self, gc):
+        if self.label != "":
+            if self._text_extents is None or len(self._text_extents) == 0:
+                self._recompute_font_metrics()
+            x,y,w,h = self._text_extents
+            gc.set_font(self.label_font)
+            text_offset = 0.0
+
+            if self.shadow_text:
+                # Draw shadow text
+                gc.set_fill_color(self.label_shadow_)
+                x_pos = self.x + (self.width-w-x)/2 + 0.5
+                y_pos = self.y + (self.height-h-y)/2 - 0.5
+                gc.show_text_at_point(self.label, x_pos, y_pos)
+                text_offset = 0.5
+            
+            # Draw foreground text to button
+            gc.set_fill_color(self.label_color_)
+            x_pos = self.x + (self.width-w-x)/2 - text_offset
+            y_pos = self.y + (self.height-h-y)/2 + text_offset
+            gc.show_text_at_point(self.label, x_pos, y_pos)
+
+        return
+
+    def normal_left_down(self, event):
+        self.button_state = "down"
+        self.request_redraw()
+        event.handled = True
+        return
+    
+    def normal_left_up(self, event):
+        self.button_state = "up"
+        self.request_redraw()
+        self.perform(event)
+        event.handled = True
+        return
+
+    def _recompute_font_metrics(self):
+        if self.label != "":
+            metrics = font_metrics_provider()
+            metrics.set_font(self.label_font)
+            self._text_extents = metrics.get_text_extent(self.label)
+
+    def _label_font_changed(self, old, new):
+        self._recompute_font_metrics()
+
+    def _label_changed(self, old, new):
+        self._recompute_font_metrics()
+
+
+class SampleButtonButton(Button):
+
+    label = Str('Sample Button')
+
+    def perform(self, event):
+        print "this button is a sample"
+        return
---
+++ enthought/enable/tools/toolbars/viewport_toolbar.py
@@ -0,0 +1,107 @@
+# Library imports
+
+# Enthought Library imports
+from enthought.enable.api import AbstractOverlay, Container, ColorTrait
+from enthought.kiva import font_metrics_provider
+from enthought.traits.api import Enum, Bool, Float, Int, Type, List
+
+# Local imports
+from toolbar_buttons import Button
+
+class ViewportToolbar(Container, AbstractOverlay):
+    """
+    ViewportToolbar is a toolbar that is attached to the top of a viewport on
+    the block canvas.
+    """
+
+    button_spacing = Int(5)
+
+    button_vposition = Int(6)
+
+    always_on = Bool(True)
+
+    toolbar_height = Float(30.0)
+
+    order = Enum("left-to-right", "right-to-left")
+    
+    buttons = List(Type(Button))
+
+    # Override default values for inherited traits
+    auto_size = False
+
+    bgcolor = ColorTrait((0.5, 0.5, 0.5, 0.25))
+
+    def __init__(self, component=None, *args, **kw):
+        # self.component should be a CanvasViewport
+        self.component = component
+        for buttontype in self.buttons:
+            self.add_button(buttontype())
+        super(ViewportToolbar, self).__init__(*args, **kw)
+
+    def _do_layout(self, component=None):
+        if component is None:
+            component = self.component
+
+        if component is not None:
+            self.x = component.x
+            # FIXME: Adding 2 to the self.y because there is a tiny gap
+            # at the top of the toolbar where components from the block 
+            # canvas show through.
+            self.y = component.y2 - self.toolbar_height + 2
+            self.height = self.toolbar_height
+            self.width = component.width
+
+        metrics = font_metrics_provider()
+        if self.order == "right-to-left":
+            last_button_position = self.width - self.button_spacing
+            for b in self.components:
+                x, y, w, h = metrics.get_text_extent(b.label)
+                b.width = w + 2*b.label_padding
+                b.x = last_button_position - b.width
+                b.y = self.button_vposition
+                last_button_position -= b.width + self.button_spacing*2
+        else:
+            last_button_position = 0
+            for b in self.components:
+                x, y, w, h = metrics.get_text_extent(b.label)
+                b.width = w + 2*b.label_padding
+                b.x = self.button_spacing + last_button_position
+                b.y = self.button_vposition
+                last_button_position += b.width + self.button_spacing*2
+
+    def overlay(self, other_component, gc, view_bounds=None, mode="normal"):
+        c = other_component
+        self.do_layout(component=c)
+        gc.save_state()
+        try:
+            gc.clip_to_rect(c.x, c.y, c.width, c.height)
+            Container._draw(self, gc, view_bounds)
+        finally:
+            gc.restore_state()
+        return
+    
+    def add_button(self, button):
+        self.add(button)
+        button.toolbar_overlay = self
+        self._layout_needed = True
+        return
+
+
+class HoverToolbar(ViewportToolbar):
+
+    # This is sort of a hack to make the container handle events like a plain
+    # container.  We also want this overlay to be "opaque", so that events
+    # inside it don't continue propagating.
+    def _dispatch_stateful_event(self, event, suffix):
+        Container._dispatch_stateful_event(self, event, suffix)
+        if not event.handled:
+            if self.is_in(event.x, event.y):
+                event.handled = True
+        return
+        
+    def _container_handle_mouse_event(self, event, suffix):
+        if not self.is_in(event.x, event.y) and self.component.auto_hide:
+            self.component.remove_toolbar()
+            self.component.request_redraw()
+        return
+
