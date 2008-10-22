2cef6760ecb5aa6897c3543ccb169521255780c0
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-10-22 16:35:19 +0000

    added simple example of using savage's SVGEditor

---
+++ examples/savage/static_image.py
@@ -0,0 +1,29 @@
+from enthought.savage.svg.document import SVGDocument
+from enthought.savage.traits.ui.wx.svg_editor import SVGEditor
+from enthought.traits.api import HasTraits, Instance
+from enthought.traits.ui.api import Item, View
+import xml.etree.cElementTree as etree
+
+class StaticImageExample(HasTraits):
+    svg = Instance(SVGDocument)
+    
+    traits_view = View(Item('svg', editor=SVGEditor(), 
+                            width=350, height=450,
+                            show_label=False),
+                       title="StaticImageExample")
+                            
+    def __init__(self, filename, *args, **kw):
+        super(StaticImageExample, self).__init__(*args, **kw)
+        
+        tree = etree.parse(filename)
+        root = tree.getroot()
+        
+        # FIXME: programatically figure out which renderer to use
+        from enthought.savage.svg.backends.wx.renderer import Renderer as WxRenderer
+        self.svg = SVGDocument(root, renderer=WxRenderer)
+        
+if __name__ == "__main__":
+    import os.path
+    sample_file = os.path.join(os.path.dirname(__file__), 'lion.svg')
+    example = StaticImageExample(sample_file)
+    example.configure_traits()
