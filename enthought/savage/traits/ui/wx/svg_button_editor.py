c352409aef37cdc62ab9397fc685662ba29e2d17
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-11-19 01:15:26 +0000

    added dimming of button when clicking on it. This resembles what Traits does, though it may not look right in apps still

--- enthought/savage/traits/ui/wx/svg_button_editor.py
+++ enthought/savage/traits/ui/wx/svg_button_editor.py
@@ -46,80 +46,6 @@ class ButtonRenderPanel(RenderPanel):
         self.Refresh()
 
 
-class _SVGButtonControl(Widget):
-    
-    document = Instance(SVGDocument)
-    
-    # The (optional) label:
-    label = Str
-    
-    # Extra padding to add to both the left and right sides:
-    width_padding = Range( 0, 31, 7 )
-    
-    # Extra padding to add to both the top and bottom sides:
-    height_padding = Range( 0, 31, 5 )
-    
-    # Presentation style:
-    style = Enum( 'button', 'radio', 'toolbar', 'checkbox' )
-    
-    # Orientation of the text relative to the image:
-    orientation = Enum( 'vertical', 'horizontal' )
-    
-    # Fired when a 'button' or 'toolbar' style control is clicked: 
-    clicked = Event
-    
-    width = Int(32)
-    height = Int(32)
-    
-    def __init__ ( self, parent, **traits ):
-        """ Creates a new image control. 
-        """
-        super( _SVGButtonControl, self ).__init__( **traits )
-                                  
-        panel = wx.Window( parent, -1, (self.width, self.height))
-                                  
-        border = self.width_padding
-        border_flag = wx.RIGHT
-        if self.orientation == 'vertical':
-            sizer = wx.BoxSizer(wx.VERTICAL)
-            border = self.height_padding
-            border_flag = wx.BOTTOM
-        else:
-            sizer = wx.BoxSizer(wx.HORIZONTAL)
-            
-        panel.SetSizer(sizer)
-        panel.SetAutoLayout(True)
-        panel.SetBackgroundColour(wx.NullColor)
-        
-        render_panel = ButtonRenderPanel(parent, self)
-        
-        if self.label != '':
-            label_control = wx.StaticText(panel, -1, self.label)
-            sizer.Add(label_control, 0, border_flag, 0)
-            
-            label_size = label_control.GetSize()
-            if self.orientation == 'vertical':
-                width = max(self.width, label_size.x + 2)
-                height = self.height + label_size.y + 2
-            else:
-                height = max(self.height, label_size.y + 2)
-                width = self.width + label_size.x + 2
-            
-            panel.SetSize((width, height))
-                                  
-        sizer.Add(render_panel, 0, border_flag, border)
-                                  
-        self.control = panel
-        self.control._owner = self
-        self._mouse_over    = self._button_down = False
- 
-        # Set up mouse event handlers:
-        #wx.EVT_ENTER_WINDOW( self.control, self._on_enter_window )
-        #wx.EVT_LEAVE_WINDOW( self.control, self._on_leave_window )
-        #wx.EVT_LEFT_DOWN(    self.control, self._on_left_down )
-        #wx.EVT_LEFT_UP(      self.control, self._on_left_up )
-        #wx.EVT_PAINT(        self.control, self._on_paint )
-
 #-------------------------------------------------------------------------------
 #  '_SVGEditor' class:
 #-------------------------------------------------------------------------------
@@ -144,8 +70,6 @@ class _SVGButtonEditor ( Editor ):
         root = tree.getroot()
         
         self.document = SVGDocument(root, renderer=Renderer)
-        #self._control = _SVGButtonControl( parent, document=document, label=self.factory.label)
-        #self.control = self._control.control
         self.control = ButtonRenderPanel( parent, self)
         
         svg_w, svg_h = self.control.GetBestSize()
@@ -161,6 +85,8 @@ class _SVGButtonEditor ( Editor ):
         """ Updates the editor when the object trait changes externally to the
             editor.
         """
+        import pdb;pdb.set_trace()
+        self.value = factory.value
         pass
         #self.control.Refresh()
                     
