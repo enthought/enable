29c15897bf8c5a66b963e7a46f1cc5735dd233a2
dmartin <dmartin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-01 19:58:22 +0000

    Ensuring the macport test is only run on darwin systems.

--- enthought/kiva/tests/macport_test.py
+++ enthought/kiva/tests/macport_test.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env pythonw
 
-import wx
+import sys
 
-from enthought.kiva.mac import get_macport
+if sys.platform == 'darwin':
+    import wx
 
-class SimpleWindow(wx.Frame):
-    """
-    Simple test of get_macport().
-    """
-    def __init__(self):
-        wx.Frame.__init__(self, parent=None, id=-1, title="foo", 
-                           pos=(100,100),
-                           size=(300,300))
-        oldstyle = self.GetWindowStyle()
-        oldstyle = oldstyle | wx.FULL_REPAINT_ON_RESIZE
-        self.SetWindowStyle(oldstyle)
-        self.Show(1)
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.memdc = wx.MemoryDC()
-        self.bitmap = wx.EmptyBitmap(200,200)
-        self.memdc.SelectObject(self.bitmap)
-        print "constructed frame"
+    from enthought.kiva.mac import get_macport
 
-    def OnPaint(self, evt):
-        dc = wx.PaintDC(self)
-        print "paintdc.this:", dc.this
-        print "paintdc.macport:", get_macport(dc)
-        print "memdc.this:", self.memdc.this
-        print "memdc.macport:", get_macport(self.memdc)
+    class SimpleWindow(wx.Frame):
+        """
+        Simple test of get_macport().
+        """
+        def __init__(self):
+            wx.Frame.__init__(self, parent=None, id=-1, title="foo", 
+                               pos=(100,100),
+                               size=(300,300))
+            oldstyle = self.GetWindowStyle()
+            oldstyle = oldstyle | wx.FULL_REPAINT_ON_RESIZE
+            self.SetWindowStyle(oldstyle)
+            self.Show(1)
+            self.Bind(wx.EVT_PAINT, self.OnPaint)
+            self.memdc = wx.MemoryDC()
+            self.bitmap = wx.EmptyBitmap(200,200)
+            self.memdc.SelectObject(self.bitmap)
+            print "constructed frame"
 
-class MyApp(wx.App):
-    def OnInit(self):
-        w = SimpleWindow()
-        return 1
+        def OnPaint(self, evt):
+            dc = wx.PaintDC(self)
+            print "paintdc.this:", dc.this
+            print "paintdc.macport:", get_macport(dc)
+            print "memdc.this:", self.memdc.this
+            print "memdc.macport:", get_macport(self.memdc)
 
-app = MyApp(False)
-app.MainLoop()
+    class MyApp(wx.App):
+        def OnInit(self):
+            w = SimpleWindow()
+            return 1
+
+    app = MyApp(False)
+    app.MainLoop()
