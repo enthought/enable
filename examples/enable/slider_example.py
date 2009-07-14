9705a3aa0e24f9f9d93530c61384b0cc739fa250
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-14 21:04:43 +0000

    adding Slider widget example

---
+++ examples/enable/slider_example.py
@@ -0,0 +1,34 @@
+
+from enthought.enable.api import Container, OverlayContainer, Slider, Window
+from enthought.enable.example_support import demo_main, DemoFrame
+
+class MyFrame(DemoFrame):
+
+    def _create_window(self):
+        slider = Slider()
+        slider.set_slider_pixels(10)
+        slider.slider_thickness = 5
+        slider.set_endcap_percent(0.1)
+        slider.min = 0
+        slider.max = 100
+        slider.value = 40
+        slider.padding = 25
+        slider.slider = "cross"
+        slider.orientation = "h"
+        slider.num_ticks = 4
+        slider.set_tick_percent(0.05)
+
+        container = OverlayContainer()
+        container.add(slider)
+
+        slider.on_trait_change(self.val_changed, "value")
+        self.slider = slider
+        return Window(self, component=container)
+
+    def val_changed(self):
+        print self.slider.value
+
+if __name__ == "__main__":
+    demo_main(MyFrame, title="Slider example")
+
+
