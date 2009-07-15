a82d571d2c48a161961d732c6d9b353714a66f0d
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-15 19:38:27 +0000

    adding simple example showing how to put something button-like in an enable canvas using svgs

---
+++ examples/savage/buttons_on_canvas.py
@@ -0,0 +1,98 @@
+import os.path
+import xml.etree.cElementTree as etree
+
+from enthought.enable.api import Container, Component, ComponentEditor, BaseTool
+from enthought.traits.api import Instance, Callable, List, Str, HasTraits
+from enthought.traits.ui.api import View, Item
+from enthought.savage.svg.document import SVGDocument
+from enthought.savage.svg.backends.kiva.renderer import Renderer as KivaRenderer
+
+
+class CanvasButton(Component):
+    document = Instance(SVGDocument)
+    callback = Callable
+    callback_args = List(Str)
+    
+    bounds = [64, 64]
+    
+    def __init__(self, filename, callback, callback_args, *args, **kw):
+        super(CanvasButton, self).__init__(*args, **kw)
+        
+        if not os.path.exists(filename):
+            raise ValueError
+        tree = etree.parse(filename)
+        root = tree.getroot()
+        self.document =  SVGDocument(root, renderer=KivaRenderer)
+        
+        self.callback = callback
+        self.callback_args = callback_args        
+    
+    def draw(self, gc, view_bounds, mode):
+        gc.save_state()
+        gc.translate_ctm(self.x, self.y+self.height)
+        doc_size = self.document.getSize()
+        gc.scale_ctm(self.width/float(doc_size[0]), -self.height/float(doc_size[1]))
+        
+        self.document.render(gc)
+        
+        gc.restore_state()
+
+    def perform(self):
+        self.callback(*self.callback_args)
+        
+
+class ButtonCanvas(Container):
+    def draw(self, gc, view_bounds=None, mode="default"):
+        for component in self.components:
+            component.draw(gc, view_bounds, mode)
+            
+    def add_button(self, button):
+        button.container = self
+        self.components.append(button)
+        
+class ButtonSelectionTool(BaseTool):
+    """ Listens for double-clicks and tries to open a traits editor on the
+        graph node under the mouse.
+    """ 
+        
+    def normal_left_down(self, event):
+        for component in self.component.components:
+            if component.is_in(event.x, event.y):
+                component.perform()
+                break
+
+        
+class ButtonCanvasView(HasTraits):
+    canvas = Instance(Container)
+    
+    traits_view = View(Item('canvas', editor=ComponentEditor(),
+                            show_label=False),
+                        width=400,
+                        height=400,
+                        resizable=True)
+    
+    def __init__(self, *args, **kw):
+        super(ButtonCanvasView, self).__init__(*args, **kw)
+        self.add_buttons()
+    
+    def _canvas_default(self):
+        """ default setter for _canvas
+        """
+        container = ButtonCanvas()
+        container.tools.append(ButtonSelectionTool(component=container))
+        return container
+
+    def add_buttons(self):
+        data_dir = os.path.dirname(__file__)
+        self.canvas.add_button(CanvasButton(os.path.join(data_dir, 'edit-copy.svg'), self.do_copy, [], x=150, y=150))
+        self.canvas.add_button(CanvasButton(os.path.join(data_dir, 'edit-paste.svg'), self.do_paste, [], x=250, y=150))
+
+    def do_copy(self):
+        print "copying something"
+        
+    def do_paste(self):
+        print "pasting something"
+        
+        
+if __name__ == "__main__":
+    ButtonCanvasView().configure_traits()
