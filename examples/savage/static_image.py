2aedad68b857740b829264c0898e707604972b12
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-16 16:34:02 +0000

    added arguments for specifying the renderer via the command line

--- examples/savage/static_image.py
+++ examples/savage/static_image.py
@@ -4,6 +4,9 @@ from enthought.traits.api import HasTraits, Instance
 from enthought.traits.ui.api import Item, View
 import xml.etree.cElementTree as etree
 
+from enthought.savage.svg.backends.wx.renderer import Renderer as WxRenderer
+from enthought.savage.svg.backends.kiva.renderer import Renderer as KivaRenderer
+
 class StaticImageExample(HasTraits):
     svg = Instance(SVGDocument)
     
@@ -12,17 +15,28 @@ class StaticImageExample(HasTraits):
                             show_label=False),
                        title="StaticImageExample")
                             
-    def __init__(self, filename, *args, **kw):
+    def __init__(self, filename, renderer, *args, **kw):
         super(StaticImageExample, self).__init__(*args, **kw)
 
-        # FIXME: programatically figure out which renderer to use
-        from enthought.savage.svg.backends.wx.renderer import Renderer as WxRenderer
-        from enthought.savage.svg.backends.kiva.renderer import Renderer as KivaRenderer
         self.svg = SVGDocument.createFromFile(filename, renderer=WxRenderer)
         
 if __name__ == "__main__":
     import os.path
     import sys
 
+    renderer = WxRenderer
+
+    if '--wx' in sys.argv:
+        renderer = WxRenderer
+        sys.argv.remove('--wx')
+    if '--kiva' in sys.argv:
+        renderer = KivaRenderer
+        sys.argv.remove('--kiva')
+ 
+
     if len(sys.argv) > 1:
-        StaticImageExample(sys.argv[1]).configure_traits(kind='live')
+        StaticImageExample(sys.argv[1], renderer).configure_traits()
+    else:
+        filename = os.path.join(os.path.dirname(__file__), 'lion.svg')
+        StaticImageExample(filename, renderer).configure_traits()
+     
