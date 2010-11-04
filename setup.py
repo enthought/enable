bc74754c45eb25db489196c6166c0e1e18cb6648
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-11-04 17:38:16 +0000

    DOC: update required Cython version

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ the Enable project:
 
 * `setuptools <http://pypi.python.org/pypi/setuptools/0.6c8>`_
 * `SWIG <http://www.swig.org/>`_ version 1.3.30 or later.
-* `Cython <http://www.cython.org>`_ version 0.12.1 or later
+* `Cython <http://www.cython.org>`_ version 0.13 or later
 * `Numpy <http://pypi.python.org/pypi/numpy/1.1.1>`_  version 1.1.0 or later is
   preferred. Version 1.0.4 will work, but some tests may fail.
 * `ReportLab Toolkit <http://www.reportlab.org/rl_toolkit.html/>`_ for PDF
