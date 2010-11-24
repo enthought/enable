b9dcca831dc35cd2412e21e84ad3e6bd23864857
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-11-24 20:52:07 +0000

    ENH: fixed wrong code indentation

--- enthought/kiva/agg/src/gl_graphics_context.h
+++ enthought/kiva/agg/src/gl_graphics_context.h
@@ -12,8 +12,8 @@
     #ifdef _MSC_VER
         #include <windows.h>
     #endif
-	#include <GL/gl.h>
-	#include <GL/glu.h>
+    #include <GL/gl.h>
+    #include <GL/glu.h>
 
     // The following mechanism is necessary in order to use MultiDrawElements
     // on Windows with mingw.
