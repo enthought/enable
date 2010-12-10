9bef6ecd8ebb78bb13a8622300837381852f71f0
jwiggins <jwiggins@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-12-10 21:45:00 +0000

    Remove commented-out code.

--- examples/kiva/gradient.py
+++ examples/kiva/gradient.py
@@ -16,7 +16,7 @@ def draw(gc):
         gc.rect(50,25,150,100)
         gc.linear_gradient(50,25,150,125,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
 
     # vertical top to bottom
@@ -24,14 +24,14 @@ def draw(gc):
         gc.rect(50,150,150,50)
         gc.linear_gradient(0,200, 0,150,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
     # horizontal left to right
     with gc:
         gc.rect(50,200,150,50)
         gc.linear_gradient(50,0, 150,0,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
 
     # vertical bottom to top
@@ -39,14 +39,14 @@ def draw(gc):
         gc.rect(50,275,150,50)
         gc.linear_gradient(0,275, 0,325,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
     # horizontal right to left
     with gc:
         gc.rect(50,325,150,50)
         gc.linear_gradient(200,0, 100,0,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
     
     # radial
@@ -54,7 +54,7 @@ def draw(gc):
         gc.arc(325, 75, 50, 0.0, 2*pi)
         gc.radial_gradient(325, 75, 50, 325, 75,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
 
     # radial with focal point in upper left
@@ -62,7 +62,7 @@ def draw(gc):
         gc.arc(325, 200, 50, 0.0, 2*pi)
         gc.radial_gradient(325, 200, 50, 300, 225,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
 
     # radial with focal point in bottom right
@@ -70,7 +70,7 @@ def draw(gc):
         gc.arc(325, 325, 50, 0.0, 2*pi)
         gc.radial_gradient(325, 325, 50, 350, 300,
                            array([starting_color, ending_color]),
-                           "pad")#, 'objectBoundingBox')
+                           "pad")
         gc.draw_path(constants.FILL)
 
     return
