342d1f9cd40e8463081d9de7ba4172e1e7efe765
ascopatz <ascopatz@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-12-23 03:23:36 +0000

    Edited sphinx configuration for Enable.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,12 +94,12 @@ html_style = 'default.css'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-#html_logo = None
+html_logo = "_static/e-logo-rev.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "et.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
