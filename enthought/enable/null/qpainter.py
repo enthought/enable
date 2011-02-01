69e022d3a50f60ecd3a4a4927d2795728a0444a0
John Wiggins <jwiggins@enthought.com>
2011-02-01 16:56:18 -0600

    Add a qpainter backend for the null toolkit

---
+++ enthought/enable/null/qpainter.py
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2011, Enthought, Inc.
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in enthought/LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
+#------------------------------------------------------------------------------
+
+from enthought.kiva.qpainter import CompiledPath, GraphicsContext, font_metrics_provider
+
+class NativeScrollBar(object):
+    pass
+
+class Window(object):
+    pass
