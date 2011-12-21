54b157d414df04a8a4da96b966b2530e1251cf8b
Ilan Schnell <ischnell@enthought.com>
2011-12-21 15:37:15 -0600

    REL: update version in preparation for release

--- enable/__init__.py
+++ enable/__init__.py
@@ -3,9 +3,9 @@
 """ A multi-platform object drawing library.
     Part of the Enable project of the Enthought Tool Suite.
 """
-__version__ = '4.0.1'
+__version__ = '4.1.0'
 
 __requires__ = [
     'traitsui',
-    'PIL'
+    'PIL',
 ]
