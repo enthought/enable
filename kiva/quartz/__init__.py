cebdd28b92281a361599d91610fc835d2658eb38
John Wiggins <jwiggins@enthought.com>
2012-06-18 15:20:10 -0500

    Catch import errors caused by the macport extension being 32-bit only.

--- kiva/quartz/__init__.py
+++ kiva/quartz/__init__.py
@@ -3,11 +3,15 @@
 # :License:   BSD Style
 
 from mac_context import get_mac_context
-from macport import get_macport as _get_macport
+try:
+    from macport import get_macport as _get_macport
+except ImportError:
+    # macport is not available on 64-bit.
+    _get_macport = None
 
 
 def get_macport(dc):
-    """
-    Returns the m_macPort of a wxDC (or child class) instance.
+    """ Returns the m_macPort of a wxDC (or child class) instance.
+        NOTE: This is only available on 32-bit at this time.
     """
     return _get_macport(str(dc.this))
