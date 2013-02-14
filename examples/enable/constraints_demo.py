9e671a9d4a878e2ba419f5f9b91f335bb2d3bd00
John Wiggins <jwiggins@enthought.com>
2013-02-14 15:56:23 -0600

    Get some of the layout helpers working, more or less...

--- examples/enable/constraints_demo.py
+++ examples/enable/constraints_demo.py
@@ -1,5 +1,6 @@
 
 from enable.api import Component, ComponentEditor, ConstraintsContainer
+from enable.layout.layout_helpers import hbox, align, grid
 from traits.api import HasTraits, Instance
 from traitsui.api import Item, View
 
@@ -29,24 +30,11 @@ class Demo(HasTraits):
 
         parent.add(one, two, three, four)
         parent.layout_constraints = [
-            parent.constraints.top == one.constraints.top,
-            parent.constraints.left == one.constraints.left,
-            parent.constraints.top == two.constraints.top,
-            parent.constraints.right == two.constraints.right,
-            parent.constraints.bottom == three.constraints.bottom,
-            parent.constraints.left == three.constraints.left,
-            parent.constraints.bottom == four.constraints.bottom,
-            parent.constraints.right == four.constraints.right,
-            one.constraints.right == two.constraints.left,
-            three.constraints.right == four.constraints.left,
-            one.constraints.bottom == three.constraints.top,
-            two.constraints.bottom == four.constraints.top,
-            one.constraints.width == two.constraints.width,
-            one.constraints.width == three.constraints.width,
-            one.constraints.width == four.constraints.width,
-            one.constraints.height == three.constraints.height,
-            one.constraints.height == two.constraints.height,
-            one.constraints.height == four.constraints.height,
+            hbox(one.constraints, two.constraints,
+                 three.constraints, four.constraints),
+            align('width', one.constraints, two.constraints,
+                           three.constraints, four.constraints),
+
         ]
 
         return parent
