6c7a0984f135e4d56fea82a4aeecebcab23aa24c
Pankaj Pandey <pankaj@enthought.com>
2013-10-28 17:53:40 +0530

    Tests: Fix tests for running on python 3.
    All unit tests now pass on python 3.3 and 2.7 on linux.
    Few integration tests segfault, as was the case before.

--- enable/savage/svg/pathdata.py
+++ enable/savage/svg/pathdata.py
@@ -31,17 +31,13 @@ class CaselessPreservingLiteral(CaselessLiteral):
         super(CaselessPreservingLiteral,self).__init__( matchString.upper() )
         self.name = "'%s'" % matchString
         self.errmsg = "Expected " + self.name
-        self.myException.msg = self.errmsg
 
     def parseImpl( self, instring, loc, doActions=True ):
         test = instring[ loc:loc+self.matchLen ]
         if test.upper() == self.match:
             return loc+self.matchLen, test
         #~ raise ParseException( instring, loc, self.errmsg )
-        exc = self.myException
-        exc.loc = loc
-        exc.pstr = instring
-        raise exc
+        raise ParseException( instring, loc, self.errmsg, self )
 
 def Sequence(token):
     """ A sequence of the token"""
