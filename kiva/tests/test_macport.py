c8d9268f45f6a79de57e23d0b792d1b7cdaa664b
John Wiggins <jwiggins@enthought.com>
2013-11-19 12:11:59 -0600

    Only run the macport test on OS X.

--- kiva/tests/test_macport.py
+++ kiva/tests/test_macport.py
@@ -1,41 +1,42 @@
-#!/usr/bin/env pythonw
-
 import sys
 
-if sys.platform == 'darwin':
-    import wx
+def test_macport():
+    if sys.platform == 'darwin':
+        import wx
 
-    from kiva.quartz import get_macport
+        from kiva.quartz import get_macport
 
-    class SimpleWindow(wx.Frame):
-        """
-        Simple test of get_macport().
-        """
-        def __init__(self):
-            wx.Frame.__init__(self, parent=None, id=-1, title="foo",
-                               pos=(100,100),
-                               size=(300,300))
-            oldstyle = self.GetWindowStyle()
-            oldstyle = oldstyle | wx.FULL_REPAINT_ON_RESIZE
-            self.SetWindowStyle(oldstyle)
-            self.Show(1)
-            self.Bind(wx.EVT_PAINT, self.OnPaint)
-            self.memdc = wx.MemoryDC()
-            self.bitmap = wx.EmptyBitmap(200,200)
-            self.memdc.SelectObject(self.bitmap)
-            print "constructed frame"
+        class SimpleWindow(wx.Frame):
+            """
+            Simple test of get_macport().
+            """
+            def __init__(self):
+                wx.Frame.__init__(self, parent=None, id=-1, title="foo",
+                                   pos=(100,100),
+                                   size=(300,300))
+                oldstyle = self.GetWindowStyle()
+                oldstyle = oldstyle | wx.FULL_REPAINT_ON_RESIZE
+                self.SetWindowStyle(oldstyle)
+                self.Show(1)
+                self.Bind(wx.EVT_PAINT, self.OnPaint)
+                self.memdc = wx.MemoryDC()
+                self.bitmap = wx.EmptyBitmap(200,200)
+                self.memdc.SelectObject(self.bitmap)
 
-        def OnPaint(self, evt):
-            dc = wx.PaintDC(self)
-            print "paintdc.this:", dc.this
-            print "paintdc.macport:", get_macport(dc)
-            print "memdc.this:", self.memdc.this
-            print "memdc.macport:", get_macport(self.memdc)
+            def OnPaint(self, evt):
+                dc = wx.PaintDC(self)
+                print "paintdc.this:", dc.this
+                print "paintdc.macport: %x" % get_macport(dc)
+                print "memdc.this:", self.memdc.this
+                print "memdc.macport: %x" % get_macport(self.memdc)
 
-    class MyApp(wx.App):
-        def OnInit(self):
-            w = SimpleWindow()
-            return 1
+                # We're done here
+                self.Close()
 
-    app = MyApp(False)
-    app.MainLoop()
+        class MyApp(wx.App):
+            def OnInit(self):
+                w = SimpleWindow()
+                return 1
+
+        app = MyApp(False)
+        app.MainLoop()
