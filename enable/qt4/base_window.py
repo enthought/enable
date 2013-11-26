2364083ae6d9e8a0c75260f463196cc3c2833227
Corran Webster <cwebster@enthought.com>
2013-11-26 15:00:42 -0600

    Fix old-style exception raising code.

--- enable/qt4/base_window.py
+++ enable/qt4/base_window.py
@@ -331,7 +331,7 @@ class _Window(AbstractWindow):
 
     def set_drag_result(self, result):
         if result not in DRAG_RESULTS_MAP:
-            raise RuntimeError, "Unknown drag result '%s'" % result
+            raise RuntimeError("Unknown drag result '%s'" % result)
         self._drag_result = DRAG_RESULTS_MAP[result]
         return
 
