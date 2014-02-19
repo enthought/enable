77ffa14b4723002c839fc9253ae7c0c1cb539744
Ioannis Tziakos <ioannist@enthought.com>
2014-02-19 13:13:25 +0000

    add tests for the KivaTestAssistant

--- kiva/testing.py
+++ kiva/testing.py
@@ -28,7 +28,7 @@ class KivaTestAssistant(object):
 
 
         """
-        gc = GraphicsContext((width, height))
+        gc = GraphicsContext((int(width), int(height)))
         gc.clear((0.0, 0.0, 0.0, 0.0))
         gc.stroke_path = Mock() if stroke_path is None else stroke_path
         gc.draw_path = Mock() if draw_path is None else draw_path
@@ -37,7 +37,9 @@ class KivaTestAssistant(object):
     def assertPathsAreProcessed(self, drawable, width=200, height=200):
         """ Check that drawing does not leave paths unused in the GC cache.
 
-        When drawing paths are created
+        The method checks that there is something drawn into the
+        graphics context and then that all the paths have been compiled
+        and processed successfully.
 
         Parameters
         ----------
@@ -45,24 +47,27 @@ class KivaTestAssistant(object):
             A drawable object that has a draw method.
 
         width : int, optional
-            The width of the array buffer
+            The width of the array buffer (default is 200).
 
         height : int, optional
-            The height of the array buffer
+            The height of the array buffer (default is 200).
 
         """
+        # check first that something is drawn to the graphics context.
         gc = GraphicsContext((width, height))
         drawable.draw(gc)
         compiled_path = gc._get_path()
+        total_vertices = compiled_path.total_vertices()
         self.assertEqual(
-            compiled_path.total_vertices(), 0,
-            msg='There are compiled paths that '
-            'have not been processed: {0}'.format(compiled_path))
+            total_vertices, 0,
+            msg='There are {0} vertices in compiled paths {1} that '
+            'have not been processed'.format(total_vertices, compiled_path))
 
     def assertPathsAreCreated(self, drawable, width=200, height=200):
         """ Check that drawing creates paths.
 
-
+        When paths and lines creation methods are used from a graphics
+        context the drawing paths are compiled and processed.
 
         Parameters
         ----------
@@ -70,10 +75,10 @@ class KivaTestAssistant(object):
             A drawable object that has a draw method.
 
         width : int, optional
-            The width of the array buffer (default is 200)
+            The width of the array buffer (default is 200).
 
         height : int, optional
-            The height of the array buffer (default is 200)
+            The height of the array buffer (default is 200).
 
         """
         gc = self.create_mock_gc(width, height)
