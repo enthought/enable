79c440305efa50f770dc11d9f26d34d252f45a20
John Wiggins <jwiggins@enthought.com>
2014-03-03 12:13:38 -0600

    Add an example which demonstrates the CTF editor gadget.

---
+++ examples/enable/gadgets/ctf_demo.py
@@ -0,0 +1,33 @@
+from traits.etsconfig.api import ETSConfig
+ETSConfig.toolkit = 'qt4'
+
+from enaml.qt.qt_application import QtApplication
+from traits.api import HasTraits, Instance
+from traitsui.api import View, UItem
+from enable.api import ComponentEditor
+
+from enable.gadgets.ctf_editor import CtfEditor
+
+
+class Demo(HasTraits):
+
+    ctf = Instance(CtfEditor)
+
+    traits_view = View(
+        UItem('ctf',
+              editor=ComponentEditor(),
+              style='custom'),
+        width=450,
+        height=150,
+        title="Color Transfer Function Editor",
+        resizable=True,
+    )
+
+
+if __name__ == "__main__":
+    ctf = CtfEditor()
+    demo = Demo(ctf=ctf)
+
+    app = QtApplication()
+    demo.edit_traits()
+    app.start()
