2b1f12d7f2e44b7bb5408e49c0064b257155d4cd
John Wiggins <jwiggins@enthought.com>
2014-04-24 19:04:27 -0500

    Create the 4.4.0 release branch.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'enable'
-copyright = '2008-2011, Enthought'
+copyright = '2008-2014, Enthought'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
