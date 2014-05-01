9fca96f7120e6dba8da4d48a97d33bdeaba5d8d2
John Wiggins <jwiggins@enthought.com>
2014-05-01 11:14:59 -0500

    Bump the version number up to 4.5.0-dev

--- enable/__init__.py
+++ enable/__init__.py
@@ -3,7 +3,7 @@
 """ A multi-platform object drawing library.
     Part of the Enable project of the Enthought Tool Suite.
 """
-__version__ = '4.4.0'
+__version__ = '4.5.0-dev'
 
 __requires__ = [
     'traitsui',
