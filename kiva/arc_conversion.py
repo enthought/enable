6ee2621e322a4cf177a79cd9e3437a7b05d58b42
Corran Webster <cwebster@enthought.com>
2014-07-12 15:03:46 -0500

    PEP8 arc_conversion.py

--- kiva/arc_conversion.py
+++ kiva/arc_conversion.py
@@ -2,6 +2,7 @@ from numpy import sqrt, dot, sin, array, pi
 
 from math import atan2, acos
 
+
 def two_point_arc_to_kiva_arc(p1, p2, theta):
     """
     Converts an arc in two point and subtended angle  format (startpoint,
@@ -12,21 +13,22 @@ def two_point_arc_to_kiva_arc(p1, p2, theta):
     chord = p2-p1
     chordlen = sqrt(dot(chord, chord))
     radius = abs(chordlen/(2*sin(theta/2)))
-    altitude = sqrt(pow(radius, 2)-pow(chordlen/2, 2))
-    if theta>pi or theta<0:
+    altitude = sqrt(pow(radius, 2) - pow(chordlen/2, 2))
+    if theta > pi or theta < 0:
         altitude = -altitude
     chordmidpoint = (p1+p2)/2
-    rotate90 = array(((0.0,-1.0),
+    rotate90 = array(((0.0, -1.0),
                       (1.0, 0.0)))
     centerpoint = dot(rotate90, (chord/chordlen))*altitude + chordmidpoint
 
-    start_angle = atan2(*(p1-centerpoint)[::-1])
-    end_angle = start_angle+theta
-    if theta<0:
+    start_angle = atan2(*(p1 - centerpoint)[::-1])
+    end_angle = start_angle + theta
+    if theta < 0:
         start_angle, end_angle, = end_angle, start_angle
     cw = False
     radius = abs(radius)
     return (centerpoint[0], centerpoint[1], radius, start_angle, end_angle, cw)
+
 
 def arc_to_tangent_points(start, p1, p2, radius):
     """ Given a starting point, two endpoints of a line segment, and a radius,
