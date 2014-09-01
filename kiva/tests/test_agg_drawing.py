006dac00dbef6851d3e000885856dc959ceb61a7
Ioannis Tziakos <ioannist@enthought.com>
2014-09-01 21:51:39 +0100

    update tests about agg drawing

--- kiva/tests/test_agg_drawing.py
+++ kiva/tests/test_agg_drawing.py
@@ -1,21 +1,18 @@
 import unittest
 
-from kiva.tests.drawing_tester import DrawingTester
+from kiva.tests.drawing_tester import DrawingImageTester
 from kiva.image import GraphicsContext
 
 
-class TestAggDrawing(DrawingTester, unittest.TestCase):
+class TestAggDrawing(DrawingImageTester, unittest.TestCase):
 
     def setUp(self):
-        DrawingTester.setUp(self)
+        DrawingImageTester.setUp(self)
         self.gc.set_stroke_color((0.0, 0.0, 0.0))
         self.gc.set_fill_color((0.0, 0.0, 1.0))
 
     def create_graphics_context(self, width, height):
         return GraphicsContext((width, height))
-
-    def save_to_file(self):
-        self.gc.save(self.filename)
 
 
 if __name__ == "__main__":
