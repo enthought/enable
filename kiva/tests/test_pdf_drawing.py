7b8d2ae2598734a740183e22ecdaaef86a877653
Ioannis Tziakos <ioannist@enthought.com>
2014-09-01 21:13:37 +0100

    support some very basic testing for the PDF backend

--- kiva/tests/test_pdf_drawing.py
+++ kiva/tests/test_pdf_drawing.py
@@ -1,8 +1,12 @@
+import contextlib
+import StringIO
 import unittest
+
+import PyPDF2  # Tests require the PyPDF2 library for parsing the pdf stream
+from reportlab.pdfgen.canvas import Canvas
 
 from kiva.tests.drawing_tester import DrawingTester
 from kiva.pdf import GraphicsContext
-from reportlab.pdfgen.canvas import Canvas
 
 
 class TestPDFDrawing(DrawingTester, unittest.TestCase):
@@ -16,8 +20,28 @@ class TestPDFDrawing(DrawingTester, unittest.TestCase):
         canvas = Canvas(self.filename, (width, height))
         return GraphicsContext(canvas)
 
-    def save_to_file(self):
-        self.gc.save()
+    @contextlib.contextmanager
+    def draw_and_check(self):
+        yield
+        # Read the pdfstream.
+        pdfdata = self.gc.gc.getpdfdata()
+        stream = StringIO.StringIO(pdfdata)
+        reader = PyPDF2.PdfFileReader(stream)
+        self.assertEqual(reader.getNumPages(), 1)
+
+        # Find the graphics in the page
+        page = reader.getPage(0)
+        content = page.getContents()
+
+        # Just a simple check that the path has been closed or the text has
+        # been drawn.
+        line = content.getData().splitlines()[-2].strip()
+        if not any((
+            line.endswith('f'),
+            line.endswith('S'),
+            line.endswith('f*'),
+            line.endswith('ET') and 'hello kiva' in line)):
+            self.fail('Path was not closed')
 
 
 if __name__ == "__main__":
