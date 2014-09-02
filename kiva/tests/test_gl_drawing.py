27451444c6784004f0fe708c7ceff30c46a6bbef
Ioannis Tziakos <ioannist@enthought.com>
2014-09-02 02:15:32 +0100

    avoid segfault in gl tests

--- kiva/tests/test_gl_drawing.py
+++ kiva/tests/test_gl_drawing.py
@@ -9,6 +9,9 @@ class TestGLDrawing(DrawingImageTester, unittest.TestCase):
     def create_graphics_context(self, width, height):
         return GraphicsContext((width, height))
 
+    def test_star_clip(self):
+        # FIXME: overriding test since it segfaults
+        self.fail('This normally segfaults')
 
 if __name__ == "__main__":
     unittest.main()
