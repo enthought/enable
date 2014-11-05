8b95b91dfcd1b807f437bdd5ddf3581f1bc289c5
John Wiggins <jwiggins@enthought.com>
2014-11-05 16:10:35 -0600

    Add some print statements to debug a Travis problem.

--- kiva/agg/tests/test_utils.py
+++ kiva/agg/tests/test_utils.py
@@ -4,4 +4,9 @@ from numpy import alltrue, ravel
 class Utils(object):
 
     def assertRavelEqual(self, x, y):
-        self.assert_(alltrue(ravel(x) == ravel(y)), "\n%s\n !=\n%s" % (x, y))
+        try:
+            self.assert_(alltrue(ravel(x) == ravel(y)), "\n%s\n !=\n%s" % (x, y))
+        except ValueError:
+            print "x:", x
+            print "y:", y
+            raise
