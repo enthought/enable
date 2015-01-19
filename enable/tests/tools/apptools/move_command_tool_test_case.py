ea6aa1571dc000a30157477332d825102e4d9c78
Corran Webster <cwebster@enthought.com>
2015-01-19 13:59:24 +0000

    Add test case using ResizeCommand.move_command.

--- enable/tests/tools/apptools/move_command_tool_test_case.py
+++ enable/tests/tools/apptools/move_command_tool_test_case.py
@@ -21,7 +21,7 @@ from traits.testing.unittest_tools import UnittestTools, unittest
 from enable.component import Component
 from enable.container import Container
 from enable.testing import EnableTestAssistant
-from enable.tools.apptools.commands import MoveCommand
+from enable.tools.apptools.commands import MoveCommand, ResizeCommand
 from enable.tools.apptools.move_command_tool import MoveCommandTool
 
 
@@ -61,14 +61,14 @@ class MoveCommandToolTestCase(unittest.TestCase, EnableTestAssistant,
         mouse_up_event = self.mouse_up(self.component, 195, 95, window=window)
         self.assertTrue(mouse_up_event.handled)
 
-        # check if a ResizeCommand was pushed onto the stack
+        # check if a MoveCommand was pushed onto the stack
         args, kwargs = self.command_stack.push.call_args
         self.assertIsNotNone(args)
         self.assertEqual(len(args), 1)
         self.assertIsInstance(args[0], MoveCommand)
         command = args[0]
 
-        # check that the ResizeCommand has right parameters
+        # check that the MoveCommand has right parameters
         self.assertEqual(command.data, (100, 0))
         self.assertEqual(command.previous_position, (50, 50))
         self.assertFalse(command.mergeable)
@@ -102,5 +102,43 @@ class MoveCommandToolTestCase(unittest.TestCase, EnableTestAssistant,
         mouse_up_event = self.mouse_up(self.component, 195, 95, window=window)
         self.assertFalse(mouse_up_event.handled)
 
-        # check if a ResizeCommand was pushed onto the stack
+        # check if a MoveCommand was pushed onto the stack
         self.assertFalse(self.command_stack.push.called)
+
+    def test_drag_resize_move_command(self):
+        self.tool.command = ResizeCommand.move_command
+
+        window = self.create_mock_window()
+
+        # start the mouse drag
+        mouse_down_event = self.mouse_down(self.component, 145, 145,
+                                           window=window)
+        #self.assertTrue(mouse_down_event.handled)
+        self.assertTrue(self.tool._mouse_down_received)
+
+        # start moving the mouse
+        mouse_move_event = self.mouse_move(self.component, 145, 145,
+                                           window=window)
+        self.assertTrue(mouse_move_event.handled)
+
+        # move the mouse to the final location
+        mouse_move_event = self.mouse_move(self.component, 195, 95,
+                                           window=window)
+        self.assertTrue(mouse_move_event.handled)
+
+        # release the mouse, ending the drag
+        mouse_up_event = self.mouse_up(self.component, 195, 95, window=window)
+        self.assertTrue(mouse_up_event.handled)
+
+        # check if a ResizeCommand was pushed onto the stack
+        args, kwargs = self.command_stack.push.call_args
+        self.assertIsNotNone(args)
+        self.assertEqual(len(args), 1)
+        self.assertIsInstance(args[0], ResizeCommand)
+        command = args[0]
+
+        # check that the ResizeCommand has right parameters
+        self.assertEqual(command.data, (100, 0, 100, 100))
+        self.assertEqual(command.previous_rectangle, (50, 50, 100, 100))
+        self.assertFalse(command.mergeable)
+        self.assertEqual(command.component, self.component)
