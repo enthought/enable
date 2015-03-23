2d04512fcbfc8e19467517da85193aea44d935d4
John Wiggins <jwiggins@enthought.com>
2015-03-23 11:05:08 -0500

    Start development of version 4.6.0

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ from numpy.distutils.core import setup
 
 
 MAJOR = 4
-MINOR = 5
+MINOR = 6
 MICRO = 0
 
-IS_RELEASED = True
+IS_RELEASED = False
 
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
