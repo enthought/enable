696d8ff0bdaed360803c410dc7162efcbcd68535
Ioannis Tziakos <ioannist@enthought.com>
2015-05-08 21:38:03 +0100

    try something else

--- travis_setup.sh
+++ travis_setup.sh
@@ -13,6 +13,7 @@ sudo ln -s /usr/lib/`uname -i`-linux-gnu/libpng.so /usr/lib/
 sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
 
 if [ "$ETS_TOOLKIT" = "wx" ]; then
+    echo "HERE"
     sudo apt-get install python-wxtools python-wxgtk2.8-dbg
 elif [ "$ETS_TOOLKIT" = "qt4" ]; then
     sudo apt-get install python-qt4 python-qt4-dev python-sip python-qt4-gl libqt4-scripttools
@@ -21,11 +22,11 @@ fi
 # compile cairo
 wget -nv http://cairographics.org/releases/py2cairo-1.10.0.tar.bz2
 tar -xf py2cairo-1.10.0.tar.bz2
-pushd py2cairo-1.10.0
+cd py2cairo-1.10.0
 ./waf configure
 ./waf build
 sudo ./waf install
-popd
+cd ..
 
 # setup X11 for the tests
 export DISPLAY=:99.0
