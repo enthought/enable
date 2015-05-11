3a600f316cd6d3baf13dac53216baad1d0e45a22
Ioannis Tziakos <ioannist@enthought.com>
2015-05-11 09:57:29 +0100

    minor cleanup

--- travis_setup.sh
+++ travis_setup.sh
@@ -14,7 +14,6 @@ sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
 
 # setup gui
 if [ "$ETS_TOOLKIT" = "wx" ]; then
-    echo "HERE"
     sudo apt-get install python-wxtools python-wxgtk2.8-dbg
 elif [ "$ETS_TOOLKIT" = "qt4" ]; then
     sudo apt-get install python-qt4 python-qt4-dev python-sip python-qt4-gl libqt4-scripttools
@@ -23,8 +22,8 @@ fi
 # compile cairo
 wget -nv http://cairographics.org/releases/py2cairo-1.10.0.tar.bz2
 tar -xf py2cairo-1.10.0.tar.bz2
-cd py2cairo-1.10.0
+pushd py2cairo-1.10.0
 ./waf configure
 ./waf build
 sudo ./waf install
-cd ..
+popd
