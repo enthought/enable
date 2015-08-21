48266ba93e4992755dc099165e43adb20c45a2ad
Corran Webster <cwebster@enthought.com>
2015-08-21 10:56:41 +0100

    Remove sudo usage (PIL may not compile properly...)

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,23 +1,23 @@
 #! /bin/sh
 set -e
 
-sudo apt-get update
+#sudo apt-get update
 
 # Install swig for agg
-sudo apt-get install swig
+#sudo apt-get install swig
 
 # Simlinks for PIL compilation
-sudo ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
-sudo ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
-sudo ln -s /usr/lib/`uname -i`-linux-gnu/libpng.so /usr/lib/
-sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
+#sudo ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
+#sudo ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
+#sudo ln -s /usr/lib/`uname -i`-linux-gnu/libpng.so /usr/lib/
+#sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
 
 # setup gui
-if [ "$ETS_TOOLKIT" = "wx" ]; then
-    sudo apt-get install python-wxtools python-wxgtk2.8-dbg
-elif [ "$ETS_TOOLKIT" = "qt4" ]; then
-    sudo apt-get install python-qt4 python-qt4-dev python-sip python-qt4-gl libqt4-scripttools
-fi
+#if [ "$ETS_TOOLKIT" = "wx" ]; then
+#    sudo apt-get install python-wxtools python-wxgtk2.8-dbg
+#elif [ "$ETS_TOOLKIT" = "qt4" ]; then
+#    sudo apt-get install python-qt4 python-qt4-dev python-sip python-qt4-gl libqt4-scripttools
+#fi
 
 # compile cairo
 wget -nv http://cairographics.org/releases/py2cairo-1.10.0.tar.bz2
