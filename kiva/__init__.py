08da07b78b12c253e45062ff8f55caa678e98961
Corran Webster <cwebster@enthought.com>
2015-09-11 16:12:01 -0500

    Explicit relative imports in kiva.__init__.

--- kiva/__init__.py
+++ kiva/__init__.py
@@ -17,8 +17,8 @@ Part of the Enable project of the Enthought Tool Suite.
 
 from kiva._version import full_version as __version__
 
-from constants import *
-from fonttools import Font
+from .constants import *
+from .fonttools import Font
 
 import os
 if os.environ.has_key('KIVA_WISHLIST'):
