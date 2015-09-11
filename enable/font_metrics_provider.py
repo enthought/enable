d919e83fc8dd2d5c913cfacfaa1a6d196054a45d
Corran Webster <cwebster@enthought.com>
2015-09-11 16:35:46 -0500

    More relative imports.

--- enable/font_metrics_provider.py
+++ enable/font_metrics_provider.py
@@ -14,7 +14,7 @@
 
 
 # Import the toolkit specific version.
-from toolkit import toolkit_object
+from .toolkit import toolkit_object
 font_metrics_provider = toolkit_object('font_metrics_provider')
 
 #### EOF ######################################################################
--- enable/kiva_graphics_context.py
+++ enable/kiva_graphics_context.py
@@ -14,7 +14,7 @@
 
 
 # Import the toolkit specific version.
-from toolkit import toolkit_object
+from .toolkit import toolkit_object
 GraphicsContext = toolkit_object('GraphicsContext')
 
 #### EOF ######################################################################
