cf9e1960fb6b42b0f6a65c82dd83a38e3f8b1f58
Ioannis Tziakos <ioannist@enthought.com>
2015-10-17 02:15:29 +0100

    correct function name

--- kiva/compat.py
+++ kiva/compat.py
@@ -10,14 +10,14 @@ PILLOW_VERSION = getattr(PIL, 'PILLOW_VERSION', PIL.VERSION)
 
 
 def piltostring(image):
-    if PILLOW_VERSION.starts_with('3'):
+    if PILLOW_VERSION.startswith('3'):
         return image.tobytes()
     else:
         return image.tostring()
 
 
 def pilfromstring(*args, **kwargs):
-    if PILLOW_VERSION.starts_with('3'):
+    if PILLOW_VERSION.startswith('3'):
         return Image.frombytes(*args, **kwargs)
     else:
         return Image.tostring(*args, **kwargs)
