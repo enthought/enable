5d89e6e14aeb7630f5e08a878fb647c2d60956cd
Jean-Mathieu Deschenes <deschenes.j.m@gmail.com>
2017-07-25 10:40:28 -0400

    Pyside Setup is apparently fixed.

--- build_pyside_wheel.sh
+++ build_pyside_wheel.sh
@@ -9,15 +9,6 @@ else
    git clone https://github.com/PySide/pyside-setup.git
    cd pyside-setup
 
-   # The normal pyside repos only have the right tags upto 1.1.1
-   # So we need to replace the repos with the newer ones
-   git submodule deinit .
-   git rm sources/pyside
-   git rm sources/shiboken
-   git submodule add --name sources/shiboken -- https://github.com/PySide/shiboken2.git sources/shiboken
-   git submodule add --name sources/pyside -- https://github.com/PySide/pyside2.git sources/pyside
-   git submodule sync
-
    # now it is time to build the pyside wheels
    python setup.py bdist_wheel --qmake=/usr/bin/qmake-qt4 --version=1.2.2 --jobs=2
    ls dist/
