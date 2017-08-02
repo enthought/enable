e3830f0ba8c896fd4f22038423bda7275035e999
Jean-Mathieu Deschenes <deschenes.j.m@gmail.com>
2017-08-02 17:17:09 -0400

    Forgot one iteritems

--- enable/trait_defs/rgba_color_trait.py
+++ enable/trait_defs/rgba_color_trait.py
@@ -82,7 +82,7 @@ convert_to_color.info = ('a tuple of the form (red,green,blue,alpha), where '
 
 # RGBA versions of standard colors
 rgba_standard_colors = {}
-for name, color in six.iteritems(standard_colors):
+for name, color in standard_colors.items():
     rgba_standard_colors[ name ] = rgba_color(color)
 rgba_standard_colors[ 'clear' ] = ( 0, 0, 0, 0 )
 
