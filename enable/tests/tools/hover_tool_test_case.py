a9286b7de5770a53e6c05e8b82f9aa3b8ac664a7
Steven Kern <skern@enthought.com>
2019-03-19 12:54:02 -0500

    TST: increase coverage of hover cases

--- enable/tests/tools/hover_tool_test_case.py
+++ enable/tests/tools/hover_tool_test_case.py
@@ -18,29 +18,56 @@ GuiTestAssistant = toolkit_object('util.gui_test_assistant:GuiTestAssistant')
 no_gui_test_assistant = (GuiTestAssistant.__name__ == 'Unimplemented')
 
 
+LOWER_BOUND = 50
+SIZE = 100
+UPPER_BOUND = LOWER_BOUND + SIZE
+MIDDLE = LOWER_BOUND + (SIZE / 2)
+LOCATIONS = [
+    ('left', LOWER_BOUND + 1, MIDDLE + 1),
+    ('top', MIDDLE - 2, UPPER_BOUND - 2),
+    ('right', UPPER_BOUND - 2, MIDDLE + 1),
+    ('bottom', MIDDLE + 1, LOWER_BOUND + 1),
+    ('borders', LOWER_BOUND + 1, LOWER_BOUND + 1),
+    ('LL', LOWER_BOUND + 1, LOWER_BOUND + 1),
+    ('UL', LOWER_BOUND + 1, UPPER_BOUND - 2),
+    ('UR', UPPER_BOUND - 2, UPPER_BOUND - 2),
+    ('LR', UPPER_BOUND - 2, LOWER_BOUND + 1),
+    ('corners', LOWER_BOUND + 1, LOWER_BOUND + 1),
+]
+
+
 @skipIf(no_gui_test_assistant, 'No GuiTestAssistant')
 class HoverToolTestCase(EnableTestAssistant, GuiTestAssistant, TestCase):
 
     def setUp(self):
         super(HoverToolTestCase, self).setUp()
-        self.component = Component(position=[50, 50], bounds=[100, 100])
+        self.component = Component(
+            position=[LOWER_BOUND, LOWER_BOUND],
+            bounds=[SIZE, SIZE],
+        )
         # add hover tool with hover zone in lower-left corner of component
         self.tool = HoverTool(component=self.component, area_type='LL')
         self.component.tools.append(self.tool)
 
     @patch('enable.tools.hover_tool.GetGlobalMousePosition')
     def test_basic_hover(self, mock_global_mouse_position):
-        tool = self.tool
-        xy_pos = (51, 51)  # mouse in lower-left corner of component
-        mock_global_mouse_position.return_value = xy_pos
-        with self.assertTraitChanges(tool, '_timer', count=1):
-            with self.assertTraitChanges(tool, '_start_xy', count=1):
-                condition = lambda: mock_global_mouse_position.call_count == 2
-                with self.event_loop_until_condition(condition):
-                    self.mouse_move(self.component, *xy_pos)
+        for i, (area_type, x_pos, y_pos) in enumerate(LOCATIONS):
+            self.component.tools = []
+            tool = HoverTool(component=self.component, area_type=area_type)
+            self.component.tools.append(tool)
+            xy_pos = (x_pos, y_pos)
+            mock_global_mouse_position.return_value = xy_pos
+            with self.assertTraitChanges(tool, '_timer', count=1):
+                with self.assertTraitChanges(tool, '_start_xy', count=1):
+                    condition = lambda: mock_global_mouse_position.call_count == 2 * (i + 1)
+                    with self.event_loop_until_condition(condition):
+                        self.mouse_move(self.component, *xy_pos)
 
-        self.assertEqual(mock_global_mouse_position.call_count, 2)
-        self.assertEqual(tool.event_state, 'normal')
+            self.assertEqual(
+                mock_global_mouse_position.call_count,
+                2 * (i + 1),
+            )
+            self.assertEqual(tool.event_state, 'normal')
 
     @patch('enable.tools.hover_tool.GetGlobalMousePosition')
     def test_out_of_hover_zone(self, mock_global_mouse_position):
