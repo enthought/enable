1820165c23e3d3d6de4d95f02f358b4a79d2c7fc
John Wiggins <plasticjeebus@gmail.com>
2019-05-30 21:42:55 +0200

    Add a test for gradient string arg typemapping

--- kiva/tests/test_agg_drawing.py
+++ kiva/tests/test_agg_drawing.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from kiva.tests.drawing_tester import DrawingImageTester
 from kiva.image import GraphicsContext
 from traits.testing.unittest_tools import unittest
@@ -7,6 +9,18 @@ class TestAggDrawing(DrawingImageTester, unittest.TestCase):
 
     def create_graphics_context(self, width, height):
         return GraphicsContext((width, height))
+
+    def test_unicode_gradient_args(self):
+        color_nodes = [(0.0, 1.0, 0.0, 0.0), (1.0, 0.0, 0.0, 0.0)]
+        with self.draw_and_check():
+            w, h = self.gc.width(), self.gc.height()
+            grad_stops = np.array([(x, r, g, b, 1.0)
+                                   for x, r, g, b in color_nodes])
+
+            self.gc.rect(0, 0, w, h)
+            self.gc.linear_gradient(0, 0, w, 0, grad_stops,
+                                    u'pad', b'userSpaceOnUse')
+            self.gc.fill_path()
 
 
 if __name__ == "__main__":
