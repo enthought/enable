9412938746d8e01c84d9d1a038ffde70530a5d14
Poruri Sai Rahul <rporuri@enthought.com>
2019-06-02 14:11:01 +0530

    FIX : Remove quotes around pillow cmdline arg
    	modified:   appveyor-run.cmd

--- appveyor-run.cmd
+++ appveyor-run.cmd
@@ -13,7 +13,7 @@ FOR %%x IN (%*) DO (
 SET /A counter=!counter! + 1
 IF !counter! EQU 1 SET operation=%%x
 IF !counter! EQU 2 SET runtime=%%x
-IF !counter! GTR 2 CALL edm run -- python ci/edmtool.py !operation! --runtime=!runtime! --toolkit=%%x --pillow='pillow'|| GOTO error
+IF !counter! GTR 2 CALL edm run -- python ci/edmtool.py !operation! --runtime=!runtime! --toolkit=%%x --pillow=pillow || GOTO error
 )
 GOTO end
 
