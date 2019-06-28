c1af12177a9284ac0aeb8a89969d919ced6e3771
John Wiggins <plasticjeebus@gmail.com>
2019-06-28 20:57:18 +0200

    Bump version to 4.8.1

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ from numpy.distutils.misc_util import is_string
 
 MAJOR = 4
 MINOR = 8
-MICRO = 0
+MICRO = 1
 
-IS_RELEASED = True
+IS_RELEASED = False
 
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
