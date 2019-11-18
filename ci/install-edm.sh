be9ddeb06f7bd7743c59ba3b1dd181d0a08906b0
John Wiggins <jwiggins@enthought.com>
2019-11-18 13:14:08 +0000

    Upgrade to EDM 2.0.0

--- ci/install-edm.sh
+++ ci/install-edm.sh
@@ -4,10 +4,10 @@ set -e
 
 install_edm() {
     local EDM_MAJOR_MINOR="$(echo "$INSTALL_EDM_VERSION" | sed -E -e 's/([[:digit:]]+\.[[:digit:]]+)\..*/\1/')"
-    local EDM_PACKAGE="edm_${INSTALL_EDM_VERSION}_linux_x86_64.sh"
+    local EDM_PACKAGE="edm_cli_${INSTALL_EDM_VERSION}_linux_x86_64.sh"
     local EDM_INSTALLER_DIR="${HOME}/.cache/download"
     local EDM_INSTALLER_PATH="${EDM_INSTALLER_DIR}/${EDM_PACKAGE}"
-    local DOWNLOAD_URL="https://package-data.enthought.com/edm/rh5_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}"
+    local DOWNLOAD_URL="https://package-data.enthought.com/edm/rh6_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}"
 
     if [ ! -e "$EDM_INSTALLER_PATH" ]; then
         mkdir -p ${EDM_INSTALLER_DIR}
