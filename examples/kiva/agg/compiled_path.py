3a34d0cb23cc91a90f1da7d2b32d23a91bbf7fca
Midhun PM <midhun@enthought.com>
2019-12-31 12:01:42 -0600

    Fix more examples

--- examples/kiva/agg/compiled_path.py
+++ examples/kiva/agg/compiled_path.py
@@ -1,15 +1,71 @@
-from numpy import array
+# CompiledPath should always be imported from the same backend as the
+# GC you are using.  In this case, we are using the image GraphicsContext
+# so we can save to disk when we're done, so we grab the CompiledPath
+# from there as well.
+
+import tempfile
+
 from kiva.image import GraphicsContext, CompiledPath
-from kiva.constants import STROKE, FILL_STROKE
+from kiva.constants import STROKE
 
-cross = CompiledPath()
-cross.scale_ctm(10.0, 10.0)
-lines = array([(0,1),(0,2),(1,2),(1,3),(2,3),(2,2),(3,2),(3,1),(2,1),
-               (2,0),(1,0),(1,1), (0,1)])
-cross.lines(lines)
+from enable.api import ConstraintsContainer, Component, ComponentEditor
+from enable.primitives.image import Image
 
-gc = GraphicsContext((400,400))
-gc.set_stroke_color((1,0,0,1))
-gc.draw_path_at_points(array([(50,50), (200,50), (50,200), (200,200)]), cross, STROKE)
-gc.save("compiled_path.png")
+from traits.api import HasTraits, Instance
+from traitsui.api import Item, View
+from numpy import array
 
+
+def compiled_path():
+    # Creating the compiled path
+    star_points = [(-20, -30),
+                   (0, 30),
+                   (20, -30),
+                   (-30, 10),
+                   (30, 10),
+                   (-20, -30)]
+
+    cross = CompiledPath()
+    cross.scale_ctm(10.0, 10.0)
+    lines = array(
+        [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (2, 2), (3, 2), (3, 1),
+         (2, 1),
+         (2, 0), (1, 0), (1, 1), (0, 1)])
+    cross.lines(lines)
+
+    gc = GraphicsContext((400, 400))
+    gc.set_stroke_color((1, 0, 0, 1))
+    gc.draw_path_at_points(array([(50, 50), (200, 50), (50, 200), (200, 200)]),
+                           cross, STROKE)
+
+    file_path = tempfile.mktemp(suffix='.jpg')
+
+    gc.save(file_path)
+
+    return file_path
+
+
+class Demo(HasTraits):
+    canvas = Instance(Component)
+
+    traits_view = View(Item('canvas', editor=ComponentEditor(),
+                            show_label=False, width=200, height=200),
+                       resizable=True)
+
+    def _canvas_default(self):
+        file_path = compiled_path()
+        image = Image.from_file(file_path, resist_width='weak',
+                                resist_height='weak')
+
+        container = ConstraintsContainer(bounds=[500, 500])
+        container.add(image)
+        ratio = float(image.data.shape[1]) / image.data.shape[0]
+        container.layout_constraints = [
+            image.left == container.contents_left,
+            image.right == container.contents_right,
+            image.top == container.contents_top,
+            image.bottom == container.contents_bottom,
+            image.layout_width == ratio * image.layout_height,
+        ]
+
+        return container
--- examples/kiva/agg/lion.py
+++ examples/kiva/agg/lion.py
@@ -5,6 +5,16 @@ import sys
 
 from kiva import agg
 
+import tempfile
+
+from kiva.image import GraphicsContext, CompiledPath
+from kiva.constants import STROKE
+
+from enable.api import ConstraintsContainer, Component, ComponentEditor
+from enable.primitives.image import Image
+
+from traits.api import HasTraits, Instance
+from traitsui.api import Item, View
 
 if sys.platform == 'win32':
     now = time.clock
@@ -13,8 +23,9 @@ else:
 
 from lion_data import get_lion
 
-def main():
-    sz = (1000,1000)
+
+def lion():
+    sz = (1000, 1000)
 
     t1 = now()
     path_and_color, size, center = get_lion()
@@ -24,10 +35,10 @@ def main():
     gc = agg.GraphicsContextArray(sz)
     t1 = now()
 
-    gc.translate_ctm(sz[0]/2.,sz[1]/2.)
+    gc.translate_ctm(sz[0] / 2., sz[1] / 2.)
     Nimages = 90
     for i in range(Nimages):
-        for path,color in path_and_color:
+        for path, color in path_and_color:
             gc.begin_path()
             gc.add_path(path)
             gc.set_fill_color(color)
@@ -35,8 +46,38 @@ def main():
             gc.fill_path()
         gc.rotate_ctm(1)
     t2 = now()
-    print('total time, sec/image, img/sec:', t2 - t1, (t2-t1)/Nimages, Nimages/(t2-t1))
-    gc.save('lion.bmp')
+    print('total time, sec/image, img/sec:', t2 - t1, (t2 - t1) / Nimages,
+          Nimages / (t2 - t1))
+    file_path = tempfile.mktemp(suffix='.bmp')
+    gc.save(file_path)
+    return file_path
+
+
+class Demo(HasTraits):
+    canvas = Instance(Component)
+
+    traits_view = View(Item('canvas', editor=ComponentEditor(),
+                            show_label=False, width=200, height=200),
+                       resizable=True)
+
+    def _canvas_default(self):
+        file_path = lion()
+        image = Image.from_file(file_path, resist_width='weak',
+                                resist_height='weak')
+
+        container = ConstraintsContainer(bounds=[500, 500])
+        container.add(image)
+        ratio = float(image.data.shape[1]) / image.data.shape[0]
+        container.layout_constraints = [
+            image.left == container.contents_left,
+            image.right == container.contents_right,
+            image.top == container.contents_top,
+            image.bottom == container.contents_bottom,
+            image.layout_width == ratio * image.layout_height,
+        ]
+
+        return container
+
 
 if __name__ == "__main__":
     main()
