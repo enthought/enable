4eac0347dcb2f7ad7cdb091932c80bdc1aa2edfa
Aaron Ayres <aayres@enthought.com>
2020-12-04 14:57:32 -0600

    some style fixes

--- enable/savage/svg/backends/kiva/tests/test_renderer.py
+++ enable/savage/svg/backends/kiva/tests/test_renderer.py
@@ -1,9 +1,11 @@
 import unittest
+
 from enable.savage.svg.backends.kiva.renderer import (
     LinearGradientBrush,
     RadialGradientBrush,
     Renderer,
 )
+
 
 class TestRenderer(unittest.TestCase):
 
@@ -21,10 +23,16 @@ class TestRenderer(unittest.TestCase):
         renderer = Renderer()
         lgb = renderer.createLinearGradientBrush(1,1,2,2,3)
         lgb.transforms.append('a')
-        self.assertEqual(renderer.createLinearGradientBrush(1,1,2,2,3).transforms, [])
+        self.assertEqual(
+            renderer.createLinearGradientBrush(1,1,2,2,3).transforms,
+            []
+        )
 
     def test_create_radial_gradient_brush(self):
         renderer = Renderer()
         rgb = renderer.createRadialGradientBrush(1,1,2,2,3)
         rgb.transforms.append('a')
-        self.assertEqual(renderer.createRadialGradientBrush(1,1,2,2,3).transforms, [])
+        self.assertEqual(
+            renderer.createRadialGradientBrush(1,1,2,2,3).transforms,
+            []
+        )
