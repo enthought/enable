6099222ec021ecdd6ffdccaa52a028fc34eaea57
Aaron Ayres <aayres@enthought.com>
2020-12-08 13:56:39 -0600

    apply suggestion from code review to rename methods check_->test_, and remove unneeded functions

--- kiva/agg/tests/test_gcmem.py
+++ kiva/agg/tests/test_gcmem.py
@@ -15,7 +15,7 @@ except ModuleNotFoundError:
 
 @unittest.skipIf(not etsdevtools_available, "test requires etsdevtools")
 class test_agg(unittest.TestCase):
-    def check_agg_mem_leak(self):
+    def test_agg_mem_leak(self):
         pre = get_mem_usage()
         gc = GraphicsContext((500,500))
         del gc
@@ -23,28 +23,10 @@ class test_agg(unittest.TestCase):
         post = get_mem_usage()
         assert (pre == post)
 
-    def check_wx_mem_leak(self):
+    def test_wx_mem_leak(self):
         pre = get_mem_usage()
         gc = GraphicsContextSystem((500,500))
         del gc
         garbagecollector.collect()
         post = get_mem_usage()
         assert (pre == post)
-
-
-
-def test_suite(level=1):
-    suites = []
-    if level > 0:
-        suites.append( unittest.makeSuite(test_agg,'check_') )
-    total_suite = unittest.TestSuite(suites)
-    return total_suite
-
-def test(level=10):
-    all_tests = test_suite(level)
-    runner = unittest.TextTestRunner()
-    runner.run(all_tests)
-    return runner
-
-if __name__ == "__main__":
-    test()
