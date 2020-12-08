ad1a541bf90a6fa6adf21653e7768418f42c3451
Aaron Ayres <aayres@enthought.com>
2020-12-08 10:23:24 -0600

    replace nose with unittest in test_component_editor

--- enable/tests/test_component_editor.py
+++ enable/tests/test_component_editor.py
@@ -1,6 +1,6 @@
 """ Test the interaction between traitsui and enable's ComponentEditor.
 """
-import nose
+import unittest
 
 from enable.component_editor import ComponentEditor
 from traits.has_traits import HasTraits
@@ -33,38 +33,33 @@ class _ComponentDialogWithSize(HasTraits):
         resizable=True
     )
 
+class TestComponentEditor(unittest.TestCase):
 
-@skip_if_null
-def test_initial_component():
-    # BUG: the initial size of an Item with ComponentEditor is zero
-    # in the Qt backend
+    @skip_if_null
+    def test_initial_component(self):
+        # BUG: the initial size of an Item with ComponentEditor is zero
+        # in the Qt backend
 
-    dialog = _ComponentDialog()
-    ui = dialog.edit_traits()
+        dialog = _ComponentDialog()
+        ui = dialog.edit_traits()
 
-    size = get_dialog_size(ui.control)
-    nose.tools.assert_greater(size[0], 0)
-    nose.tools.assert_greater(size[1], 0)
-
-
-@skip_if_null
-def test_initial_component_with_item_size():
-    # BEH: the initial component size should respect the size of the
-
-    dialog = _ComponentDialogWithSize()
-    ui = dialog.edit_traits()
-
-    size = get_dialog_size(ui.control)
-
-    # leave a few pixel of margin for wx
-    nose.tools.assert_greater(size[0], ITEM_WIDTH-1)
-    nose.tools.assert_less(size[0], ITEM_WIDTH+30)
-
-    nose.tools.assert_greater(size[1], ITEM_HEIGHT-1)
-    nose.tools.assert_less(size[1], ITEM_HEIGHT+30)
+        size = get_dialog_size(ui.control)
+        self.assertGreater(size[0], 0)
+        self.assertGreater(size[1], 0)
 
 
-if __name__ == '__main__':
-    # Executing the file opens the dialog for manual testing
-    vw = _ComponentDialogWithSize()
-    vw.configure_traits()
+    @skip_if_null
+    def test_initial_component_with_item_size(self):
+        # BEH: the initial component size should respect the size of the
+
+        dialog = _ComponentDialogWithSize()
+        ui = dialog.edit_traits()
+
+        size = get_dialog_size(ui.control)
+
+        # leave a few pixel of margin for wx
+        self.assertGreater(size[0], ITEM_WIDTH-1)
+        self.assertLess(size[0], ITEM_WIDTH+30)
+
+        self.assertGreater(size[1], ITEM_HEIGHT-1)
+        self.assertLess(size[1], ITEM_HEIGHT+30)
