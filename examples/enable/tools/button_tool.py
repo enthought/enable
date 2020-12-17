0f50632e1d28cf65a89b5133a0e1cefda5d35e16
Aaron Ayres <aayres@enthought.com>
2020-12-17 09:33:55 -0600

    fix incorrect docstring

--- examples/enable/tools/button_tool.py
+++ examples/enable/tools/button_tool.py
@@ -7,12 +7,8 @@
 #
 
 """
-Undoable Move Tool
-==================
-
-This example shows how to integrate a simple component move tool with pyface
-undo/redo infrastructure.
-
+Button Tool
+===========
 """
 
 from traits.api import Bool
