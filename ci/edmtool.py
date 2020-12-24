824672a2dcb74de3e91a3dfc8af36d4449c1eafe
Corran Webster <cwebster@enthought.com>
2020-12-24 11:17:06 +0000

    Remove old versions of Pillow from CI testing.

--- ci/edmtool.py
+++ ci/edmtool.py
@@ -99,6 +99,7 @@ dependencies = {
     "numpy",
     "pygments",
     "pyparsing",
+    "pillow",
     "reportlab",
     "swig",
     "traits",
@@ -136,8 +137,6 @@ environment_vars = {
     'null': {'ETS_TOOLKIT': 'null.image'},
 }
 
-pillow_trans = ''.maketrans({'<': '_', '=': '_', '>': '_'})
-
 if sys.platform == 'darwin':
     dependencies.add('Cython')
 
@@ -166,18 +165,17 @@ def cli():
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--toolkit', default='null')
-@click.option('--pillow', default='pillow')
 @click.option('--environment', default=None)
 @click.option(
     "--source/--no-source",
     default=False,
     help="Install ETS packages from source",
 )
-def install(runtime, toolkit, pillow, environment, source):
+def install(runtime, toolkit, environment, source):
     """ Install project and dependencies into a clean EDM environment.
 
     """
-    parameters = get_parameters(runtime, toolkit, pillow, environment)
+    parameters = get_parameters(runtime, toolkit, environment)
     parameters['packages'] = ' '.join(
         dependencies | extra_dependencies.get(toolkit, set()))
     # edm commands to setup the development environment
@@ -186,7 +184,6 @@ def install(runtime, toolkit, pillow, environment, source):
          "--force --version={runtime}"),
         ("edm --config {edm_config} install -y -e {environment} {packages} "
         "--add-repository enthought/lgpl"),
-        "edm run -e {environment} -- pip install {pillow}",
         ("edm run -e {environment} -- pip install -r ci/requirements.txt"
          " --no-dependencies"),
     ]
@@ -245,11 +242,10 @@ def install(runtime, toolkit, pillow, environment, source):
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--toolkit', default='null')
-@click.option('--pillow', default='pillow')
 @click.option('--environment', default=None)
-def docs(runtime, toolkit, pillow, environment):
+def docs(runtime, toolkit, environment):
     """ Build documentation. """
-    parameters = get_parameters(runtime, toolkit, pillow, environment)
+    parameters = get_parameters(runtime, toolkit, environment)
     packages = " ".join([
         "sphinx",
         "enthought_sphinx_theme",
@@ -315,14 +311,13 @@ def docs(runtime, toolkit, pillow, environment):
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--toolkit', default='null')
-@click.option('--pillow', default='pillow')
 @click.option('--environment', default=None)
-def shell(runtime, toolkit, pillow, environment):
+def shell(runtime, toolkit, environment):
     """ Create a shell into the EDM development environment
     (aka 'activate' it).
 
     """
-    parameters = get_parameters(runtime, toolkit, pillow, environment)
+    parameters = get_parameters(runtime, toolkit, environment)
     commands = [
         "edm shell -e {environment}",
     ]
@@ -332,13 +327,12 @@ def shell(runtime, toolkit, pillow, environment):
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--toolkit', default='null')
-@click.option('--pillow', default='pillow')
 @click.option('--environment', default=None)
-def test(runtime, toolkit, pillow, environment):
+def test(runtime, toolkit, environment):
     """ Run the test suite in a given environment with the specified toolkit.
 
     """
-    parameters = get_parameters(runtime, toolkit, pillow, environment)
+    parameters = get_parameters(runtime, toolkit, environment)
     environ = environment_vars.get(toolkit, {}).copy()
     environ['PYTHONUNBUFFERED'] = "1"
     commands = [
@@ -362,13 +356,12 @@ def test(runtime, toolkit, pillow, environment):
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--toolkit', default='null')
-@click.option('--pillow', default='pillow')
 @click.option('--environment', default=None)
-def cleanup(runtime, toolkit, pillow, environment):
+def cleanup(runtime, toolkit, environment):
     """ Remove a development environment.
 
     """
-    parameters = get_parameters(runtime, toolkit, pillow, environment)
+    parameters = get_parameters(runtime, toolkit, environment)
     commands = [
         "edm run -e {environment} -- python setup.py clean",
         "edm environments remove {environment} --purge -y",
@@ -381,14 +374,14 @@ def cleanup(runtime, toolkit, pillow, environment):
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--toolkit', default='null')
-@click.option('--pillow', default='pillow')
-def test_clean(runtime, toolkit, pillow):
+def test_clean(runtime, toolkit):
     """ Run tests in a clean environment, cleaning up afterwards
 
     """
-    args = ['--toolkit={}'.format(toolkit),
-            '--runtime={}'.format(runtime),
-            '--pillow={}'.format(pillow)]
+    args = [
+        '--toolkit={}'.format(toolkit),
+        '--runtime={}'.format(runtime),
+    ]
     try:
         install(args=args, standalone_mode=False)
         test(args=args, standalone_mode=False)
@@ -399,13 +392,12 @@ def test_clean(runtime, toolkit, pillow):
 @cli.command()
 @click.option('--runtime', default='3.6')
 @click.option('--toolkit', default='null')
-@click.option('--pillow', default='pillow')
 @click.option('--environment', default=None)
-def update(runtime, toolkit, pillow, environment):
+def update(runtime, toolkit, environment):
     """ Update/Reinstall package into environment.
 
     """
-    parameters = get_parameters(runtime, toolkit, pillow, environment)
+    parameters = get_parameters(runtime, toolkit, environment)
     commands = [
         "edm run -e {environment} -- python setup.py install"]
     click.echo("Re-installing in  '{environment}'".format(**parameters))
@@ -429,19 +421,18 @@ def test_all():
 # Utility routines
 # ----------------------------------------------------------------------------
 
-def get_parameters(runtime, toolkit, pillow, environment):
+def get_parameters(runtime, toolkit, environment):
     """Set up parameters dictionary for format() substitution
     """
-    parameters = {'runtime': runtime, 'toolkit': toolkit, 'pillow': pillow,
+    parameters = {'runtime': runtime, 'toolkit': toolkit,
                   'environment': environment}
     if toolkit not in supported_combinations[runtime]:
-        msg = ("Python {runtime}, toolkit {toolkit}, and pillow {pillow} "
+        msg = ("Python {runtime} and toolkit {toolkit} "
                "not supported by test environments")
         raise RuntimeError(msg.format(**parameters))
     if environment is None:
         tmpl = 'enable-test-{runtime}-{toolkit}'
         environment = tmpl.format(**parameters)
-        environment += '-{}'.format(str(pillow).translate(pillow_trans))
         parameters['environment'] = environment
 
     parameters["edm_config"] = EDM_CONFIG
