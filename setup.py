96e046d810dc6b4af2197dafa5b135dad25d03a3
Poruri Sai Rahul <rporuri@enthought.com>
2021-02-03 09:48:40 +0000

    Provide the right path to the version file on windows (#563)
    * FIX : Provide the right path to the version file on windows
    
    On the main branch, the _version.py files dont get generated in enable
    and kiva - because of the path provided to write_version_py function.
    The changes made here enable the generation of the version file - which
    also fixes an issue where documentation build is broken on windows
    because of the lack of the version file.
    
    	modified:   setup.py
    
    * FIX : Just use os.path.join instead of using separate paths for
    win/not win
    
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -405,8 +405,9 @@ def macos_extensions():
 
 
 if __name__ == "__main__":
-    write_version_py(filename='enable/_version.py')
-    write_version_py(filename='kiva/_version.py')
+    write_version_py(filename=os.path.join('enable', '_version.py'))
+    write_version_py(filename=os.path.join('kiva', '_version.py'))
+
     from enable import __version__, __extras_require__, __requires__
 
     with open('README.rst', 'r') as fp:
