6fd0daa69ab19b5ba8a86b76e618dc160d01dede
John Wiggins <jwiggins@users.noreply.github.com>
2021-02-11 20:03:26 +0100

    Pyface-style toolkit selection (#571)

--- enable/toolkit.py
+++ enable/toolkit.py
@@ -17,63 +17,23 @@
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 #
 # -----------------------------------------------------------------------------
-
-# Standard library imports.
-import sys
-from traceback import format_exception_only
-
-# Enthought library imports.
+from pyface.base_toolkit import find_toolkit
 from traits.etsconfig.api import ETSConfig
 
 
-# This is set to the api module path for the selected backend.
-_toolkit_backend = None
-
-
 def _init_toolkit():
-    """ Initialise the current toolkit. """
-
+    """ Initialise the current toolkit.
+    """
     if not ETSConfig.toolkit:
         # Force Traits to decide on its toolkit if it hasn't already
         from traitsui.api import toolkit as traits_toolkit
 
         traits_toolkit()
 
-    # Import the selected backend
-    backend = "enable.%s.%s" % (ETSConfig.toolkit, ETSConfig.kiva_backend)
-    try:
-        __import__(backend)
-    except (ImportError, SystemExit):
-        t, v, _tb = sys.exc_info()
-        raise ImportError(
-            "Unable to import the %s backend for the %s "
-            "toolkit (reason: %s)."
-            % (
-                ETSConfig.kiva_backend,
-                ETSConfig.toolkit,
-                format_exception_only(t, v),
-            )
-        )
-
-    # Save the imported toolkit module.
-    global _toolkit_backend
-    _toolkit_backend = backend
-
 
 # Do this once then disappear.
 _init_toolkit()
 del _init_toolkit
 
-
-def toolkit_object(name):
-    """ Return the toolkit specific object with the given name. """
-
-    try:
-        be_obj = getattr(sys.modules[_toolkit_backend], name)
-    except AttributeError:
-        raise NotImplementedError(
-            "the %s.%s enable backend doesn't implement %s"
-            % (ETSConfig.toolkit, ETSConfig.kiva_backend, name)
-        )
-
-    return be_obj
+# The toolkit_object function.
+toolkit_object = find_toolkit("enable.toolkits")
