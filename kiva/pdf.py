e5a99b9dd4449d7ab5a809b9a458b9d64f75ed50
John Wiggins <jwiggins@enthought.com>
2021-03-04 14:58:07 +0100

    Keep PDF save()'s arguments optional

--- kiva/pdf.py
+++ kiva/pdf.py
@@ -783,5 +783,5 @@ class GraphicsContext(GraphicsContextBase):
             # erase the current path.
             self.current_pdf_path = None
 
-    def save(self, filename, file_format=None, pil_options=None):
+    def save(self, filename='', file_format=None, pil_options=None):
         self.gc.save()
