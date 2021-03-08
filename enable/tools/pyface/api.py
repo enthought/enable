fe82ef761499f070aec26730298fa35c9a0c317e
John Wiggins <jwiggins@users.noreply.github.com>
2021-03-08 20:12:49 +0100

    Add ContextMenuTool to enable.tools.pyface.api (#690)

--- enable/tools/pyface/api.py
+++ enable/tools/pyface/api.py
@@ -10,6 +10,7 @@
 """ API for enable.tools.pyface subpackage.
 
 - :class:`~.ComponentCommand`
+- :class:`~.ContextMenuTool`
 - :class:`~.MoveCommand`
 - :class:`~.ResizeCommand`
 - :class:`~.BaseCommandTool`
@@ -21,6 +22,7 @@
 # Support for Undo/Redo with Enable
 from .commands import ComponentCommand, MoveCommand, ResizeCommand
 from .command_tool import BaseCommandTool, BaseUndoTool
+from .context_menu_tool import ContextMenuTool
 from .move_command_tool import MoveCommandTool
 from .resize_command_tool import ResizeCommandTool
 from .undo_tool import UndoTool
