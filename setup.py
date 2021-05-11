f520c3caaf096c398e699bae5e4d6262ab4a22dc
John Wiggins <jwiggins@enthought.com>
2021-05-11 18:11:04 +0200

    Nitpicky formatting

--- setup.py
+++ setup.py
@@ -432,14 +432,16 @@ def verify_swig_version():
     """ Verify we are using swiig version 3.0.x.
     """
     msg = ("SWIG is a required build dependency of Enable. Furthermore, there "
-          "is currently a known issue with SWIG 4.0, see enthought/enable#360."
-          " Please install SWIG 3.0.x (see http://www.swig.org/).")
+           "is currently a known issue with SWIG 4.0, "
+           "see enthought/enable#360. Please install SWIG 3.0.x "
+           "(see http://www.swig.org/).")
     try:
-        with subprocess.Popen(["swig", "-version"], \
-                stdout=subprocess.PIPE, encoding='utf-8') as proc:
+        cmd = ["swig", "-version"]
+        opts = {"stdout": subprocess.PIPE, "encoding": "utf-8"}
+        with subprocess.Popen(cmd, **opts) as proc:
             # We expect text along the lines of "SWIG Version 3.X.Y"
             swig_version_match = re.search(
-                '(SWIG Version 3)\.\d{1,2}\.\d{1,2}',
+                r"(SWIG Version 3)\.\d{1,2}\.\d{1,2}",
                 proc.stdout.read()
             )
             if swig_version_match is None:
@@ -450,7 +452,7 @@ def verify_swig_version():
 
 if __name__ == "__main__":
     verify_swig_version()
-    
+
     # Write version modules as needed
     enable_version_path = os.path.join('enable', '_version.py')
     write_version_py(filename=enable_version_path)
