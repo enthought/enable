dac71ae64bb3052cf9b72ac7ee19247c0f63a4db
aaronayres35 <36972686+aaronayres35@users.noreply.github.com>
2021-06-21 05:32:29 -0700

    Update enable/example_application.py
    Co-authored-by: Poruri Sai Rahul <rporuri@enthought.com>

--- enable/example_application.py
+++ enable/example_application.py
@@ -18,7 +18,7 @@ sit in enable/examples.  This module is kept for backwards compatibility.
 """
 import warnings
 
-from enable.examples.example_application import DemoApplication
+from enable.examples.example_application import DemoApplication, demo_main
 
 warnings.warn(
     "Importing from this module is deprecated, and this module will be"
