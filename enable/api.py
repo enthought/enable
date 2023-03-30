6e52d752cc257bb8f3242e92ed2686a586606d49
Chengyu Liu <cyliu@aus552cyliu.local>
2023-03-30 11:05:39 +0900

    add kiwisolver not install warning for import error

--- enable/api.py
+++ enable/api.py
@@ -283,6 +283,7 @@ from .stacked_container import HStackedContainer, VStackedContainer
 try:
     import kiwisolver
 except ImportError:
+    print("kiwisolver not imported successfully imported. Please check it is installed.")
     pass
 else:
     from .constraints_container import ConstraintsContainer
