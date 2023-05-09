37325910fb6ccdbbf8f8aa42c6366b4fca052112
Corran Webster <cwebster@enthought.com>
2023-05-09 15:58:01 +0100

    Make image backend import from oldagg rather than the other way around.

--- enable/qt/oldagg.py
+++ enable/qt/oldagg.py
@@ -7,12 +7,47 @@
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 #
 # Thanks for using Enthought open source!
-from .image import (
-    CompiledPath, font_metrics_provider, GraphicsContext, NativeScrollBar,
-    Window
-)
 
-__all__ = [
-    "CompiledPath", "GraphicsContext", "NativeScrollBar", "Window",
-    "font_metrics_provider"
-]
+from pyface.qt import QtCore, QtGui
+from kiva.agg import CompiledPath, GraphicsContextSystem as GraphicsContext
+
+from .base_window import BaseWindow
+from .scrollbar import NativeScrollBar
+
+
+class Window(BaseWindow):
+    def _create_gc(self, size, pix_format="bgra32"):
+        gc = GraphicsContext(
+            (size[0] + 1, size[1] + 1),
+            pix_format=pix_format,
+            base_pixel_scale=self.base_pixel_scale,
+            # We have to set bottom_up=0 or otherwise the PixelMap will
+            # appear upside down in the QImage.
+            bottom_up=0,
+        )
+        gc.translate_ctm(0.5, 0.5)
+
+        return gc
+
+    def _window_paint(self, event):
+        if self.control is None:
+            return
+
+        # self._gc is an image context
+        w = self._gc.width()
+        h = self._gc.height()
+        data = self._gc.pixel_map.convert_to_argb32string()
+        image = QtGui.QImage(data, w, h, QtGui.QImage.Format_ARGB32)
+        rect = QtCore.QRectF(
+            0, 0, w / self._gc.base_scale, h / self._gc.base_scale
+        )
+        painter = QtGui.QPainter(self.control)
+        painter.drawImage(rect, image)
+
+
+def font_metrics_provider():
+    from kiva.api import Font
+
+    gc = GraphicsContext((1, 1))
+    gc.set_font(Font())
+    return gc
--- kiva/oldagg.py
+++ kiva/oldagg.py
@@ -7,12 +7,33 @@
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 #
 # Thanks for using Enthought open source!
-from kiva.image import (
-    CompiledPath, font_metrics_provider, FontType, GraphicsContext,
-    GraphicsContextSystem, Image
-)
+""" This backend supports Kiva drawing into memory buffers.
 
-__all__ = [
-    "CompiledPath", "font_metrics_provider", "FontType", "GraphicsContext",
-    "GraphicsContextSystem", "Image"
-]
+    Though this can be used to perform drawing in non-GUI applications,
+    the Image backend is also used by many of the GUI backends to draw into
+    the memory space of the graphics contexts.
+"""
+
+# Soon the Agg subpackage will be renamed for real.  For now, just
+# proxy the imports.
+from .agg import GraphicsContextArray as GraphicsContext
+
+# FontType will be unified with the Kiva FontType soon.
+from .agg import AggFontType as FontType
+
+# GraphicsContextSystem wraps up platform- and toolkit- specific low
+# level calls with a GraphicsContextArray.  Eventually this low-level code
+# will be moved out of the Agg subpackage, and we won't have be importing
+# this here.
+from .agg import GraphicsContextSystem, Image
+
+# CompiledPath is an object that can efficiently store paths to be reused
+# multiple times.
+from .agg import CompiledPath
+
+
+def font_metrics_provider():
+    """ Create an object to be used for querying font metrics.
+    """
+
+    return GraphicsContext((1, 1))
